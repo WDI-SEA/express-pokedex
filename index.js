dd8b255a2c0cec80d31d2a866129b1167d786a7d
Brian Hague <bhague1281@gmail.com>
2016-06-08 13:58:45 -0700

    created scaffold for pokemon

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+
+  request(pokemonUrl, function(error, response, body) {
+    var pokemon = JSON.parse(body).results;
+    res.render('index', { pokemon: pokemon });
+  });
 });
+
+app.use('/pokemon', require('./controllers/pokemon'));
 
 var server = app.listen(process.env.PORT || 3000);
 
