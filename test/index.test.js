2746b96f2c841ec90d2362e905ecdc65e469a785
Brian Hague <bhague1281@gmail.com>
2016-06-08 12:52:23 -0700

    deleted unneeded files, renamed package

--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var request = require('supertest');
-var app = require('../index');
-
-describe('App', function() {
-  it('should return a 200 response', function(done) {
-    request(app).get('/').expect(200, done);
-  });
-});
