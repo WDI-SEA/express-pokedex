bf6f5145b8164f90adadb8cd2f02d69c41ebecf2
darkenvy <darkenvy6@gmail.com>
2016-06-16 18:44:58 -0700

    Basic functionality working

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll({})
+    .then(function(poke) {
+      res.render('./pokemon/index', { poke: poke } );
+    })
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+
+  query = {
+    pokemon: req.body.name
+  };
+  db.pokemon.create(query)
+    .then(function(newPoke) {
+      res.redirect('/');
+    });
+
 });
 
 module.exports = router;
