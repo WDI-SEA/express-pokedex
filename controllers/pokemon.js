ed50b087c639fd3147f38cdb911dc845e1c446e3
Chris Madison <CKMadison4@gmail.com>
2016-06-16 19:39:15 -0700

    Completed the deliverable.

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll({
+  }).done(function(pokeFav) {
+  res.render('pokemon.ejs', { Favorites: pokeFav });
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).done(function(pokeName) {
+  res.redirect('/pokemon');
+  });
 });
+
 
 module.exports = router;
