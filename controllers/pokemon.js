8d7e729199570d6b4bd38391095fae0a1ff625cb
Travis Smith <smithtm90@gmail.com>
2016-06-16 22:51:30 -0700

    Works, but needs a little more styling

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+
+  db.pokemon.findAll({
+  }).then(function(pokemons) {
+    res.render('favorites.ejs', { favorites: pokemons})
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+  db.pokemon.create(req.body).then(function(newPokemon) {
+    res.redirect('/pokemon');
+  });
 });
 
 module.exports = router;
