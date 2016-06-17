ba1966eceb1bb2674ac71042924ce24fff9271fc
Ivar Lee <ivarr5@gmail.com>
2016-06-16 21:42:54 -0700

    init commit

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../models');
+
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(favorites){
+// findAll() returns an array
+    res.render('favorites.ejs', {pokemon: favorites});
+  })
 });
-
-// POST - receive the name of a pokemon and add it to the database
+// POST - takes the name of the selected pokemon and adds it to the DB
 router.post('/', function(req, res) {
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(newFavorite){
+    res.redirect('/pokemon')
+  })
 });
 
 module.exports = router;
