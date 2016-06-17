3b37d62333e718d44195dd94cf467ed84e88c302
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-16 18:28:06 -0700

    basics

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(function(pokemon){
+    res.render('pokemon/index.ejs', {favorites: pokemon});
+  })
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+   db.pokemon.create({
+    name: req.body.name
+  });
+    res.redirect('/pokemon');
 });
 
 module.exports = router;
