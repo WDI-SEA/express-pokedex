61b7bde396a57e669b8b1843c26517298472805b
Thomas <evans_760@yahoo.com>
2016-06-20 10:30:20 -0700

    pokedex.

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var database = require("../models");
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+  database.pokemon.findAll().then(function(allPokemon) {
+    //res.send(allPokemon)
+     res.render('pokemon', { pokemon: allPokemon });
+  });
+
+  // res.send('Render a page of favorites here');
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+  database.pokemon.create({
+  name: req.body.name
+}).then(function(newPokemon) {
+  console.log(newPokemon.get());
+  res.redirect('/pokemon')
+});
+
+
+
 });
 
 module.exports = router;
