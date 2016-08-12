322688d20124c8749306519f996f7069a6eb02a9
zaturnvalley <zacharycmason@gmail.com>
2016-08-12 08:54:43 -0700

    routed pokemon page, but not logging on click. api was down

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var db = require('./models');
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use('/', express.static(__dirname + '/static/'));
 
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=365';
 
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
     res.render('index', { pokemon: pokemon });
   });
+});
+
+db.favoritePokemon.create({
+  name: 'Pikachu'
+}).then(function(newPokemon) {
+  console.log(snewPokemon.get());
 });
 
 app.use('/pokemon', require('./controllers/pokemon'));
