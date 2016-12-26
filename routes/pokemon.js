0e731bb9d993f78e65a6c8e133da574bb42e5d02
Donnie Ellstrom <dsvene@gmail.com>
2016-12-26 15:16:03 -0800

    Attach pokemon database to API output, store favorite pokemon name in database on request

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
+var path = require('path');
 
 // GET - return a page with favorited Pokemon
 router.get('/pokemon', function(req, res) {
@@ -8,8 +10,11 @@ router.get('/pokemon', function(req, res) {
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
   console.log(req.body.name);
+  db.pokemon.create(req.body).then(function(pokemon){
+    console.log("pokemon added to db favorites:", req.body.name);
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
