42e59014b451be6612e829a97c8a52e065a8a5d1
Ryne Schillinger <ryneschillinger@gmail.com>
2016-12-31 13:32:14 -0800

    added details page and delete functionality

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
+// REQUIREMENTS
 var express = require('express');
 var request = require('request');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var pokemonRoute = require("./routes/pokemon");
+
+// APP VARIABLES
 var db = require("./models"); 
 var app = express();
 
+function getPokemon() {
+  var fileContents = fs.readFileSync('./pokemon.json');
+  var pokemon = JSON.parse(fileContents);
+  return pokemon;
+}
+
+// SET/USE STATEMENTS
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static('public'))
-app.use(express.static('files'))
+app.use('/pokemon', pokemonRoute);
+app.use(express.static('public'));
 
+
+// ROUTES
 
 // GET - show list of available Pokemon
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  // // If accessing the API link directly:
+  // var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=100';
+  // request(pokemonUrl, function(error, response, body) {
+  //   var pokemon = JSON.parse(body).results;
+  //   res.render('index', { pokemon: pokemon });
 
-  request(pokemonUrl, function(error, response, body) {
-    var pokemon = JSON.parse(body).results;
-    res.render('index', { pokemon: pokemon });
-  });
+  res.render('index', { pokemon: getPokemon() });
+
 });
 
 // GET - return a page with favorited Pokemon
@@ -37,6 +53,13 @@ app.post('/', function(req, res) {
 	}); 
 });
 
+// GET - return a details page for each favorited Pokemon
+app.get('/pokemon/:id', function(req, res) {
+	db.pokemon.findById(req.params.id).then(function(pokemon) {
+		res.render('pokemon-detail', {pokemon: pokemon});
+	});
+});
+
 // DELETE - remove Pokemon from list of favorites
 app.delete("/pokemon/:id", function(req,res){
 	db.pokemon.findById(req.params.id).then(function(pokemon) {
@@ -44,9 +67,6 @@ app.delete("/pokemon/:id", function(req,res){
 		res.send({message:"successfully deleted"});
 	});
 });
-
-
-app.use('/pokemon', require('./routes/pokemon'));
 
 
 var server = app.listen(process.env.PORT || 3000);
