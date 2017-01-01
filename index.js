213a01263f1d64780e013231027d963ded7e601c
Tommy-Lim <tlim24@gmail.com>
2016-12-31 17:10:09 -0800

    added main.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
