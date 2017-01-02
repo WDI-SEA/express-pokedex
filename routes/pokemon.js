755884dff9199669c0175759f708e6c42275ebbc
Donnie Ellstrom <dsvene@gmail.com>
2017-01-01 20:58:11 -0800

    Add stats and abilities to details page, add basic styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -23,7 +23,12 @@ router.get('/info/:name', function(req, res){
     var height = JSON.parse(body).height * 10;
     var weight = JSON.parse(body).weight / 10;
     var sprites = JSON.parse(body).sprites;
-    res.render('info', {pokemon: pokemon, height: height, weight: weight, sprites: sprites});
+    var abilities = JSON.parse(body).abilities;
+    var stats = JSON.parse(body).stats;
+    console.log(stats);
+    console.log(stats[0]['stat']['name']);
+    console.log(stats[0]['base_stat'])
+    res.render('info', {pokemon: pokemon, height: height, weight: weight, sprites: sprites, abilities: abilities, stats: stats});
   });
 });
 
