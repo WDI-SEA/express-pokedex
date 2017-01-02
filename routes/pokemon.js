e9e293dca79e2e86b39aba837fa1b3c598cf9d9e
weirui222 <congcongli@live.com>
2017-01-02 11:39:57 -0800

    Bonus 2 - show image of pokemons

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var db = require('../models');
 // GET - return a page with favorited Pokemon
@@ -12,6 +13,15 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   db.pokemon.create(req.body).then(function(pokemon) {
     res.redirect('/pokemon');
+  });
+});
+
+router.get('/:name', function(req, res) {
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + req.params.name + '/';
+
+  request(pokemonUrl, function(error, response, body) {
+    var pokemon = JSON.parse(body);
+    res.render('pokemon', { pokemon: pokemon });
   });
 });
 
