36c4dcffa30d4cd02f1438d09f546563b7924b46
CannedYeti <cpotebnya@gmail.com>
2017-02-25 13:30:43 -0800

    started playing around with pokedex

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  res.send('Render a page of favorites here');
+	res.render('favorites')
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  res.send(req.body);
+	console.log(req.body);
+  db.favorite.create(req.body)
+  	.then(function() {
+  		res.redirect('./favorites');
+  })
 });
 
 module.exports = router;
