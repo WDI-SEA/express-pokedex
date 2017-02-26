1c923d9af48dfb4034edb4ef99fc3cd568ec09a8
jnhustin <jnhustin@gmail.com>
2017-02-25 21:18:51 -0800

    cleaned up code, added & connected SQL db

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
+// global variables and requires
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
+// set and use statements
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
-app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
-
-  request(pokemonUrl, function(error, response, body) {
-    var pokemon = JSON.parse(body).results;
-    res.render('index', { pokemon: pokemon });
-  });
-});
-
 app.use('/pokemon', require('./routes/pokemon'));
 
+// routes
+app.get('/', function(req, res) {
+	var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+	var s = {
+		limit: 25
+	}
+	request({
+  		url: pokemonUrl,
+  		qs: s
+	}, function(error, response, body) {
+    	var pokemon = JSON.parse(body).results;
+    	res.render('index', { pokemon: pokemon });
+  	});
+});
+
+
+// listen
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+$('document').ready(function() {
+	console.log('everything all set and ready to go');
+})
