7cb0c882ebc04bb7d5786d046bf10b7b8cbcabb0
jnhustin <jnhustin@gmail.com>
2017-02-26 13:11:25 -0800

    connected get/post request to db, views render correctly

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('.././models');
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-	console.log(req.body);
-	res.render('favorites');
+	db.pokemon.findAll()
+	.then(function(result) {
+		res.render('favorites', {result: result});
+	})
+	.catch(function(error) {
+		res.send('there has been an error');
+	})
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-	console.log('this should pass pokemon name', req.body);
-	res.render('favorites');
+
+	db.pokemon.create(req.body)
+	.then(function(favs){
+		// res.redirect('favorites');
+		res.status(200);
+	});
 });
 
 
