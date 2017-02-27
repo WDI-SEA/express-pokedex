a3d86e9abd27b4e51a07c094195f37dffa714468
themartinsjoseph <themartinsjoseph@gmail.com>
2017-02-27 08:31:07 -0800

    base route operating

--- index.js
+++ index.js
@@ -11,29 +11,33 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.use('/pokemon', require('./routes/pokemon'));
 
 //Routes
 app.get('/', function(req, res) {
-  res.render('index');
-});
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151';
 
-app.get('/pokemon', function(req, res) {
-  var pokemon = {
-      name: req.query.name
-  };
-
-  request({
-  	url: 'http://pokeapi.co/api/v2/pokemon/?limit=151',
-  	qs: pokemon
-  }, function(error, response, body){
-  		var pokemon = JSON.parse(body).results;
-  		res.render('index', { pokemon: pokemon });
+  request(pokemonUrl, function(error, response, body) {
+    var pokemon = JSON.parse(body).results;
+    res.render('index', { pokemon: pokemon });
   });
 });
 
 
+// app.get('/pokemon', function(req, res) {
+//   var pokemon = {
+//       name: req.query.name
+//   };
+
+//   request({
+//   	url: 'http://pokeapi.co/api/v2/pokemon/?limit=151',
+//   	//qs: pokemon
+//   }, function(error, response, body){
+//   		var pokemon = JSON.parse(body).results;
+//   		res.render('index', { pokemon: pokemon });
+//   });
+// });
 
 
 //Listen 'nodemon'
