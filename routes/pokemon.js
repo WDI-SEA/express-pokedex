56a884cda423adfee6a9729381a0e9f2c5c9dfdf
JRgerv <jack.gervais@gmail.com>
2017-02-26 17:08:47 -0800

    ran db migrate

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.send('Render a page of favorites here');
 });
-//          ^^change to res.render
+//          ^^change to res.render and display data from sql db
+
 
 
 // POST - receive the name of a pokemon and add it to the database
