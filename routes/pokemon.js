3109aef4fc0629de154cc88cc247ca14858502a7
jigglor <andrewtsuji@gmail.com>
2017-04-21 21:14:13 -0700

    got most of it working

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,37 @@
+var db = require('../models');
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(myFavorites) {
+        res.render('favorites', { pokemon: myFavorites });
+    }).catch(function(error) {
+        res.status(404).send(error);
+    });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+    db.pokemon.create({
+        name: req.body.name
+    }).then(function() {
+        res.status(303).redirect('/pokemon');
+    }).catch(function(error) {
+        res.status(404).send(error);
+    });
 });
+
+router.get('/:id', function(req, res) {
+    var name = req.params.id;
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + name;
+
+    request(pokemonUrl, function(error, response, body) {
+        var pokemon = JSON.parse(body);
+        res.render('pokeview', { pokemon: pokemon });
+    });
+});
+
 
 module.exports = router;
