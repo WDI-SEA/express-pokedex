1adf84bbd904360644035ba74a3bbcb9f93f9a99
alainabuzas <alaina.buzas@gmail.com>
2017-04-22 15:42:41 -0700

    working on step 3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var db = require("./models");
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -15,6 +16,41 @@ app.get('/', function(req, res) {
     request(pokemonUrl, function(error, response, body) {
         var pokemon = JSON.parse(body).results;
         res.render('index', { pokemon: pokemon });
+    });
+});
+
+
+app.get('/pokemon', function(req, res) {
+    db.pokemon.findAll().then(function(pokemon) {
+        res.render('index', { pokemon: pokemon });
+    });
+});
+
+app.post('/pokemon', function(req, res) {
+    var newPokemon = req.body;
+
+    db.pokemon.create({
+        name: newPokemon.name,
+    }).then(function(game) {
+        res.status(404).redirect('/pokemon/');
+    })
+});
+
+app.get('/pokemon/:id', function(req, res) {
+    var idOfPokemon = req.params.id;
+
+    db.pokemon.findOne({
+        where: {
+            id: idOfPokemon
+        }
+    }).then(function(pokemon) {
+        if (pokemon) {
+            res.render('pokemon-info', { pokemon: pokemon });
+        } else {
+            res.status(404).send("This particular Pokemon does not exist")
+        }
+    }).catch(function(error) {
+        res.status(404).send(error);
     });
 });
 
