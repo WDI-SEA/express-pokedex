83ddcde11c1087a3e7b08fa3965a18c3c946fffd
stevenkeiser <stevekeiser@gmail.com>
2017-04-22 17:22:05 -0700

    all but the final tweaks

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,50 @@
 var express = require('express');
 var router = express.Router();
+// var rowdy = require('rowdy-logger');
+var db = require('../models');
 
-// GET - return a page with favorited Pokemon
+var request = require('request');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var app = express();
+
+app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+// rowdy.begin(app);
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(pokemon) {
+        res.render('favorites', { pokemon: pokemon });
+    }).catch(function(error) {
+        res.status(404).send(error);
+    });
 });
 
-// POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+    var newPokemon = req.body;
+
+    var value = {
+        name: newPokemon.name
+    };
+
+
+
+    db.pokemon.create(value).then(function(pokemon) {
+        res.status(303).redirect('/pokemon');
+
+    }).catch(function(error) {
+        res.status(404).send(error);
+    });
 });
 
+router.get('/:id', function(req, res) {
+    var name = req.params.id;
+    var url = 'http://pokeapi.co/api/v2/pokemon/' + name;
+
+    request(url, function(error, response, body) {
+        var pokemon = JSON.parse(body);
+        res.render('details', { pokemon: pokemon });
+    });
+});
 module.exports = router;
