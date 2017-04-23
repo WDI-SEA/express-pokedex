e597f32a2f44fd505711e3962f7ca52736e274ca
sdkevorkian <sdkevorkian@gmail.com>
2017-04-22 19:25:55 -0700

    MVP is made, next is the styling

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+// function deletePokemon(e) {
+    //     console.log("clicked");
+    //     e.preventDefault();
+    //     var url = $(this).attr("href");
+
+    //     $.ajax({
+    //         url: url,
+    //         method: 'DELETE',
+    //     }).done(function() {
+    //         window.location('/pokemon');
+    //     });
+    // }
+
+
+    // $(function() {
+    //     $('.delete-pokemon').on("click", deletePokemon);
+    // });
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,90 @@
 var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var db = require('../models');
+var request = require('request');
+var path = require("path");
+var methodOverride = require('method-override');
 var router = express.Router();
+
+var app = express();
+
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
+// from this dude: http://stackoverflow.com/questions/34926876/override-method-get-to-delete-in-nodejs-using-anchor-tag
+router.use(function(req, res, next) {
+    if (req.query._method == 'DELETE') {
+
+        req.method = 'DELETE';
+        req.url = req.path;
+    }
+    next();
+});
+
+
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(favoritePokemon) {
+        res.render('favorites', { pokemon: favoritePokemon });
+    }).catch(function(err) {
+        res.send("oops there was an error:" + err);
+    });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
-    res.send(req.body);
+    var pokemon = req.body.name;
+
+    db.pokemon.create({
+        name: pokemon
+    }).then(function() {
+        res.redirect('/pokemon');
+    });
+
+});
+//shows info of pokemon when click on from index
+router.get('/:id', function(req, res) {
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + req.params.id;
+    request(pokemonUrl, function(err, response, body) {
+        if (err) {
+            console.log(err);
+        }
+
+        var pokemon = JSON.parse(body);
+
+        res.render('pokemon', pokemon);
+    });
+});
+
+router.get('/:name', function(req, res) {
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + req.params.name;
+    request(pokemonUrl, function(err, response, body) {
+        if (err) {
+            console.log(err);
+        }
+
+        var pokemon = JSON.parse(body);
+
+        res.render('pokemon', pokemon);
+
+    });
+});
+
+router.delete('/:name', function(req, res) {
+    var pokemonToDelete = req.params.name;
+
+    db.pokemon.destroy({
+        where: {
+            name: pokemonToDelete
+        }
+    }).then(function() {
+        res.status(204).redirect('/pokemon');
+    });
 });
 
 module.exports = router;
