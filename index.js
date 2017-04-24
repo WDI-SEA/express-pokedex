7459373797829a3908776e4afa612d367903d289
maria-christina-sevilla <m.christina.sevilla@gmail.com>
2017-04-23 19:15:27 -0700

    get post show

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var db = require("./models"); //not sure if I need this line
+var rowdy = require('rowdy-logger');
+
 var app = express();
+rowdy.begin(app);
+
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
-app.get('/', function(req, res) {
-    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
 
+// *********************************GET /ROOT HOMEPAGE *******************************************************
+app.get('/', function(req, res) { //root (homepage)
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+    //Pokemon Template
     request(pokemonUrl, function(error, response, body) {
-        var pokemon = JSON.parse(body).results;
-        res.render('index', { pokemon: pokemon });
+        var pokemon = JSON.parse(body).results; //storing object in a variable called pokemon
+        res.render('index', { pokemon: pokemon }); // {object: value}
     });
 });
 
+// ******************************LINKS TO ROUTER************************************************************************
+
 app.use('/pokemon', require('./routes/pokemon'));
 
-var server = app.listen(process.env.PORT || 3000);
+// ********************************EXECUTE THIS PROCESS TO SERVER AT START PAGE*********************************
+var server = app.listen(process.env.PORT || 3000, function() {
+    rowdy.print();
+});
 
 module.exports = server;
