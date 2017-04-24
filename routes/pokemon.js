4dfc87f45d4ac863f9da7340bfa140f855647ddb
buffalocheck <shawn@buffalocheck.com>
2017-04-23 19:22:05 -0700

    Sun night. having difficulty w/ this

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
