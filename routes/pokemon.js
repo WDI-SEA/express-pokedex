16b2bdd4ef74f60d1437ded026dfeb83f33c2b48
jblneely <bowman.neely@gmail.com>
2017-04-24 09:04:10 -0700

    homework

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+
+
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
     res.send('Render a page of favorites here');
+    db.favorite.findAll({
+
+    }).then(function(favorite) {
+        res.render('', { favorite: name });
+    });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
     res.send(req.body);
+    var newFavoriteName = req.body;
+    var newName = {
+        name: newFavoriteName.name
+    };
+
+    db.favorite.create(newName).then(function(favorite) {
+        res.status(303).redirect('/favorite');
+    }).catch(function(error) {
+        res.status(404).send(error);
+    });
 });
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    db.favorite.findOne({
+        where: {
+            id: id
+        }
+    }).then(function(favorite) {
+        var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + favorite.name;
+        request(pokemonUrl, function(error, response, body) {
+
+
+        }).catch(function(error) {
+            res.status(404).send('Not Found');
+        });
+    });
+
+    router.delete('/:id', function(req, res) {
+        console.log('delete' + req.params.id);
+        var id = req.params.id;
+        db.favorite.destroy({
+            where: {
+                id: id
+            }
+        }).then(function(favorite) {
+            res.redirect('/favorite');
+
+        }).catch(function(error) {
+            res.send('error');
+        });
+    });
+});
+
+res.redirect.body
+
+
 
 module.exports = router;
