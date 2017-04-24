72bd535fae546180b409f3161c69b82966eca7da
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-04-24 08:35:42 -0700

    finished homework

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+var db = require('./models');
+
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=21';
 
     request(pokemonUrl, function(error, response, body) {
         var pokemon = JSON.parse(body).results;
+        console.log(pokemon);
         res.render('index', { pokemon: pokemon });
     });
 });
@@ -23,3 +28,9 @@ app.use('/pokemon', require('./routes/pokemon'));
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
+route_info = app._router.stack // registered routes
+    .filter(r => r.route) // take out all the middleware
+    .map(r => r.route.path);
+
+console.log(route_info)
---
+++ migrations/20170421220533-create-pokemon.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('pokemons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      pokemon_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('pokemons');
+  }
+};
