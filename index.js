49bfa1a408fd9c11519bcaf4dca642dcbd750dc1
laurenperez <laurenashleymarks@gmail.com>
2017-09-24 13:56:27 -0700

    min req done

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
 var app = express();
 
-
-
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res) {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,49 +1,64 @@
 //this is just like index.js but for all the pokemon routes
 var express = require('express');
+var request = require('request');
 var router = express.Router(); //this just configures my routes
 var db = require('../models');
 
+
 var saveToDb = function(nameofPokemon){
-  db.pokemon.create({
-    name: nameofPokemon
-  }).then(function(data){
-    console.log(data);
+  db.pokemon.findOrCreate({
+    where: {
+      name: nameofPokemon
+    }
+  }).spread(function(data, created){
+    console.log('Find or create new Pokemon');
   });
 };
 
-var selectAllFromDb = function(){
-  db.pokemon.findAll().then(allPokemon => {
-    return allPokemon;
-  });
+var trimPokemonObject = function(pokemon) {
+  var result = {
+    name: pokemon.forms[0].name,
+    types: [],
+    abilities: [],
+    sprite: pokemon.sprites.front_default
+  }
+  for (var i = 0; i < pokemon.types.length; i++) {
+    result.types.push(pokemon.types[i].type.name)
+  }
+  for (var i = 0; i < pokemon.abilities.length; i++) {
+    result.abilities.push(pokemon.abilities[i].ability.name)
+  }
+  return result;
 };
-
-var selectOneFromDb = function(nameOfPokemon){
-  db.pokemon.find({
-  where: {name: nameOfPokemon}
-}).then(function(data) {
-  console.log(data);
-});
-};
-
 
 
 // GET - return a page with favorited Pokemon
 router.get('/mypokedex', function(req, res) {
-    var myPokemon = selectAllFromDb();
-    console.log('00000000000000000' + myPokemon);
+  db.pokemon.findAll().then(function(myPokemon) {
     res.render('favorites', {myPokemon: myPokemon});
+  });
+});
+
+
+router.get('/mypokedex/:name', function(req,res) {
+  var pokemonName = req.params.name;
+
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + pokemonName;
+  request(pokemonUrl, function(error, response, body) {
+      var pokemonData = JSON.parse(body);
+      var pokemon = trimPokemonObject(pokemonData);
+      console.log(pokemon);
+      res.render('show', {pokemon: pokemon});
+  });
 });
 
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     var myNewPokemon = req.body.name;
-    console.log(req.body.name)
     saveToDb(myNewPokemon);
     res.redirect('/pokemon/mypokedex');
 });
-
-
 
 
 
