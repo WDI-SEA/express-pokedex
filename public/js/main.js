ec7dbe498e62fd44c318717884cb017c8c864181
laurenperez <laurenashleymarks@gmail.com>
2017-09-24 23:18:18 -0700

    styling done needs redirect help

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 console.log("JS is actually working!");
+
+$('.delete-pokemon').on('click', function(e){
+  e.preventDefault();
+  var element = $(this);
+  var url = element.attr('data-delete');
+  $.ajax({
+      method: 'DELETE',
+      url: url
+  }).done(function(data) {
+    console.log(data);
+    window.location = '/mypokedex';
+  });
+});
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,28 +5,35 @@ var router = express.Router(); //this just configures my routes
 var db = require('../models');
 
 
+//SAVE pokemon to db
 var saveToDb = function(nameofPokemon){
   db.pokemon.findOrCreate({
     where: {
       name: nameofPokemon
     }
   }).spread(function(data, created){
-    console.log('Find or create new Pokemon');
-  });
+    console.log('Found or created new Pokemon');
+  })
 };
 
+
+//MAKE Pokemon object more manageble
 var trimPokemonObject = function(pokemon) {
   var result = {
     name: pokemon.forms[0].name,
     types: [],
     abilities: [],
-    sprite: pokemon.sprites.front_default
+    sprite: pokemon.sprites.front_default,
+    moves: []
   }
   for (var i = 0; i < pokemon.types.length; i++) {
     result.types.push(pokemon.types[i].type.name)
   }
   for (var i = 0; i < pokemon.abilities.length; i++) {
     result.abilities.push(pokemon.abilities[i].ability.name)
+  }
+  for (var i = 0; i < 3; i++) {
+    result.moves.push(pokemon.moves[i].move.name)
   }
   return result;
 };
@@ -40,9 +47,9 @@ router.get('/mypokedex', function(req, res) {
 });
 
 
+//GET - info from api to match whats in db
 router.get('/mypokedex/:name', function(req,res) {
   var pokemonName = req.params.name;
-
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + pokemonName;
   request(pokemonUrl, function(error, response, body) {
       var pokemonData = JSON.parse(body);
@@ -61,14 +68,15 @@ router.post('/', function(req, res) {
 });
 
 
-//to delete later
-db.user.destroy({
-  where: {firstName: 'Jordan'}
-}).then(function() {
-  //do something
-});
-
-
+//DELETE
+router.delete('/mypokedex/delete/:name', function(req, res) {
+    db.pokemon.destroy({
+      where: {name: req.params.name}
+    }).then(function() {
+      console.log('Deleted a pokemon');
+    });
+    res.redirect('/');
+  });
 
 
 
