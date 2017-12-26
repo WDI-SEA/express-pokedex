7612f0474a5b5a2de3a6c26af946cb92bfc89854
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2017-12-25 19:59:41 -0800

    checkpoint 1, stil building

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var morgn = require('morgan');
+var db = require('./models');
 var app = express();
+
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', function(req, res) {
     var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
@@ -18,7 +22,7 @@ app.get('/', function(req, res) {
     });
 });
 
-app.use('/pokemon', require('./routes/pokemon'));
+app.use('/pokemons', require('./routes/pokemons'));
 
 var server = app.listen(process.env.PORT || 3000);
 
---
+++ migrations/create-pokemon.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('favorites');
+  }
+};
