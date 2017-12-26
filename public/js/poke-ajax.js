c59af60b0ede485cbac96edd0afa111a0ef551a8
philpatt <phillip.s.patterson1@gmail.com>
2017-12-26 12:43:26 -0800

    favorites page started

---
+++ public/js/poke-ajax.js
@@ -0,0 +1,1 @@
+// delete pokemon here
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,15 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 // GET - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get('/favorites', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
+    res.render('pokemon/favorites');
 });
+
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
-    res.send(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
