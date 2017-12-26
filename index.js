1abb5a1a187958ef21a18d1d430b62de03080822
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2017-12-26 11:54:07 -0800

    working favorites and search function

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
 
     request(pokemonUrl, function(error, response, body) {
+    	console.log(body);
         var pokemon = JSON.parse(body).results;
+        var url = JSON.parse(body).results;
         res.render('index', { pokemon: pokemon });
     });
 });
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,42 @@
 var express = require('express');
+var request = require('request');
+var fs = require('fs');
+var models = require('../models');
 var router = express.Router();
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
-});
+    models.pokemon.findAll().then(function(pokemon) {
+    res.render('pokemon/favorites', {
+      pokemon: pokemon });
+  });
+  
+})
+
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
-    res.send(req.body);
+    models.pokemon.create({ name: req.body.name }).then(function() {
+    res.redirect('/');
+  });
 });
 
+// GET -  return a page with more information about a specific pokemon
+router.get('/:id', function(req, res) {
+	var pokeURL = "http://pokeapi.co/api/v2/pokemon/" + req.params.id;
+	request(pokeURL, function(error, response, body) {
+		var data = JSON.parse(body)
+		var form = data.forms
+		console.log('body', form[0].name);
+		//console.log('body', form[name]);
+    	
+        //var pokemon = JSON.parse(body).results;
+        res.render('pokemon/single');
+    });
+
+})
+
 module.exports = router;
+
