2527f68764bbc24ad45f6415cf9da86618d9a1f2
rallysport206 <phan.tony125@gmail.com>
2017-12-27 16:06:29 -0800

    something got updated and I forgot what

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
+// var db = require('../modeles');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(favPoke){
+      res.send('Render a page of favorites here');
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
+  db.pocket_monster.create(req.body).then(function(addPoke){
+    // res.redirect('/pokemon/' -)
+  })
     res.send(req.body);
 });
 
