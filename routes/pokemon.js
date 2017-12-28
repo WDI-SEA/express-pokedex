d3955af63a45c03dc12bbfe6e057830f8a6f26bc
rallysport206 <phan.tony125@gmail.com>
2017-12-27 18:11:11 -0800

    able to add pokemon to fav

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../modeles');
+var db = require('../models');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    db.pokemon.findAll().then(function(favPoke){
-      res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(pokemon){
+      res.render('./index',{pokemon:pokemon});
   });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
-  db.pocket_monster.create(req.body).then(function(addPoke){
-    // res.redirect('/pokemon/' -)
-  })
-    res.send(req.body);
+  db.pokemon.create(req.body).then(function(pokemon){
+    res.redirect('/pokemon/')
+  }).catch(function(err){
+    res.send('nope', err);
+  });
+    // res.send(req.body);
+});
+router.get('/:id', function(req, res){
+  db.pokemon.findById(req.params.id).then(function(poke){
+    res.render('pokemon', {pokemon:poke});
+  });
 });
 
 module.exports = router;
