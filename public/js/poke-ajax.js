9e7bdeaee27bfe55aeed0d780c5579f499b9c793
jaderosse <jademrosse@gmail.com>
2017-12-28 11:06:35 -0800

    routes completed. need style.

---
+++ public/js/poke-ajax.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+	$("#delete").on("click", function(e){
+		e.preventDefault();
+		console.log("deleting");
+	});
+
+});
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var db = require('../models');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+   	db.pokemon.findAll().then(function(pokemon){
+    	res.render('pokemon/index', {pokemon: pokemon});
+	});
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+    db.pokemon.create(req.body).then(function(){
+    	res.redirect('/pokemon');
+    }); 
 });
+
+router.get('/:id', function(req, res){
+	// db.pokemon.findById(req.params.id).then(function(){
+	// console.log(req.params);
+ 	db.pokemon.findOrCreate({
+ 		where: { id: req.params.id }
+ 	}).spread(function(pokemon, created){
+ 		var name = pokemon.name
+ 	
+ 		console.log(name);
+	// var name = 'bulbasaur';
+	var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + name + '/';
+
+    request({
+    	url: pokemonUrl,
+    	}, function(error, response, body) {
+        	var dataObj = JSON.parse(body);
+        	res.render('pokemon/show', { results: dataObj} );
+   		});
+    });
+});
+// });
+
+//DELETE - delete from faves by name
+// router.delete('/:name', function(req, res){
+// 	res.send('DELETE ROUTE');
+// });
 
 module.exports = router;
