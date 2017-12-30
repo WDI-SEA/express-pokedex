a05cc690323e199f25b3ef2f322864a1087e23e0
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2017-12-29 23:08:35 -0800

    updated landing page styling - working on single pokemon stlying. Stuck on random background generation

--- public/js/slideShow.js
+++ public/js/slideShow.js
@@ -1,3 +1,5 @@
+// To View angles of Pokemon
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -15,3 +17,36 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block"; 
 }
+
+// To Change from Back to front of Card
+
+function flipCard(){
+	document.getElementById('pokecard').style.display = 'block';
+	document.getElementById('back-pokecard').style.display = 'none';
+}
+
+// Load Page Content
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('pokecard').style.display = 'none';
+});
+
+/// Random BackDrop for Pokemon
+
+var backgrounds = ["/img/sprite_back1.jpeg", "/img/sprite_back2.jpg", "/img/sprite_back3.jpg"];
+
+function getRandomImage(){
+    var rnd = Math.floor(Math.random() * backgrounds.length);
+    return backgrounds[rnd];
+}
+
+function changeImage(){
+    var img = document.querySelector(".w3-content");
+    img.src = getRandomImage();
+}
+
+function init(){
+    var el = document.querySelector(".w3-content");
+    el.onclick = changeImage;
+}
+
+window.onload = init;
