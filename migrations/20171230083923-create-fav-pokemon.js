072a28e562322eddfac935d56c30c1bf72697d09
Admin <admin@Admins-MacBook-Pro.local>
2017-12-30 00:40:50 -0800

    updated check for existing items in db

--- migrations/20171230083923-create-fav-pokemon.js
+++ migrations/20171230083923-create-fav-pokemon.js
@@ -11,7 +11,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      index:{
+      index: {
         type: Sequelize.INTEGER
       },
       createdAt: {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,23 +4,43 @@ var db = require("../models");
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // res.render('pokemon', {pokemon:{name:req.body.name,index:req.body.url}});
-	db.fav_pokemon.findAll().then(function(favPokemon){
-		res.render('pokemon',{results: favPokemon});
-		
-	});
+    db.fav_pokemon.findAll().then(function(favPokemon) {
+        res.render('pokemon', {
+            results: favPokemon
+        });
+    });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
     var n = req.body.url;
-	n=parseInt(n.substring(n.length-2,n.length-1));
-	req.body.url=n;
-   	db.fav_pokemon.create({name:req.body.name,index:req.body.url}).then(function(ceatedItem){
-   		res.redirect("/pokemon/");//redirects to favorites
-	}).catch(function(err){
-		res.send("error");
-	});
+    n = parseInt(n.substring(n.length - 2, n.length - 1));
+    req.body.url = n;
+    //check db for existing items:
+    db.fav_pokemon.findAll().then(function(favPokemon) {
+    	i=true;
+    	favPokemon.forEach(function(item){
+    		if(n===item.index){
+    			i =  false;
+    			return false;
+    		}else{
+    			i=true;
+    		}
+    	});
+    	if(i===true){
+        db.fav_pokemon.create({
+            name: req.body.name,
+            index: req.body.url
+        }).then(function(ceatedItem) {
+            res.redirect("/pokemon/"); //redirects to favorites
+        }).catch(function(err) {
+            res.send("error");
+        });
+    }else{
+    	res.send("was already in db");
+    }
+    });
 });
 
 module.exports = router;
