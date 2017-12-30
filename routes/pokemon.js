5abcef20459c441cb9a96a0ff6ebf27630d3fdd4
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-29 20:22:55 -0800

    id route working adding more data to display next

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,39 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var db = require('../models');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(pokemon){
+      res.render('index', {pokemon: pokemon});
+      // res.send('Render a page of favorites here');
+    });
 });
+
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
-    res.send(req.body);
+    db.pokemon.create(req.body).then(function(createdFavorite){
+    // redirect to pokemon page
+    res.redirect('/pokemon');
+    // catch to display any error messages 
+    }).catch(function(err){
+      res.send('Uh oh!', err);
+    });
 });
+
+
+// GET - renders a page with additional info on a specific/ fav'd pokemon
+router.get('/:id', function(req, res){
+  db.pokemon.findById(req.params.id).then(function(pokemon){
+    res.render('show', {pokemon: pokemon});
+  });
+});
+
+
+
 
 module.exports = router;
