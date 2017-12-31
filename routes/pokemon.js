e2a2b6016d276e57d9abd5904bb52e38612042fa
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-12-31 11:50:02 -0800

    MVP PLUS BONUS

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var db = require("../models");
 
@@ -27,15 +28,22 @@ db.pokemon.create(req.body).then(function(pokemon){
 });
 
 router.get('/:id', function(req,res){
-	db.pokemon.findById(req.params.id).then(function(pokemon) {
-		if (pokemon) {
-			res.render('./pokemon/show', {pokemon: pokemon});
-		} else {
-			res.status(404).send('error');
-		}
-	}).catch(function(err) {
-		res.status(500).send('error');
-	});
+    db.pokemon.findById(req.params.id).then(function(pokemon){
+        if(pokemon){
+            var pokeURL = 'http://pokeapi.co/api/v2/pokemon/'+pokemon.name+'/';
+            console.log(pokeURL);
+            request(pokeURL,function(error,response,body){
+                var pokemon = JSON.parse(body);
+                console.log(pokemon);
+                res.render('pokemon/show', { pokemon: pokemon });
+            });
+        }else{
+            res.status(404).send('error in the if');
+        }
+    }).catch(function(err){
+        res.status(500).send('error to do request');
+        console.log(err);
+    });
 });
 
 
