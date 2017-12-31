99aab3584108512ecbc9724e9359fc9603525d26
adamredwoods <adamredwoods@users.noreply.github.com>
2017-12-31 00:20:49 -0800

    added info page

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,6 +4,9 @@ var db = require("../models");
 var request = require('request');
 
 var POKEMON_URL = "http://pokeapi.co/api/v2/pokemon/";
+var lastreq = "";
+var cacheName = "";
+var cacheInfo = {};
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -19,7 +22,18 @@ router.get("/:id", function(req,res) {
    //single pokemon view
    db.pokemon.findById(req.params.id).then(
       function(poke) {
-         res.render("pokemon/info", {poke:poke});
+         if (cacheName===poke.name) {
+            console.log("..using cache");
+            res.render("pokemon/info", {pokemon:cacheInfo});
+         } else {
+            request(POKEMON_URL+poke.name, function(error, response, body) {
+               cacheName = poke.name;
+               var pokemon = JSON.parse(body);
+               cacheInfo = pokemon;
+               // console.log(pokemon);
+               res.render("pokemon/info", {pokemon:pokemon});
+            });
+         }
       }
    ).catch(
       function(err) {
@@ -31,26 +45,28 @@ router.get("/:id", function(req,res) {
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
    //-- grab info first
+
+   lastreq = req.body.name;
    request(POKEMON_URL+req.body.name, function(error, response, body) {
       var pokemon = JSON.parse(body);
       let obj = {
          "name": pokemon.name,
+         "pokeid": pokemon.id,
          "baseexp": pokemon.base_experience,
          "imageurl": pokemon.sprites.front_default
       }
-
+      lastreq="";
       db.pokemon.create(obj).then(
          function(result) {
             res.redirect("/pokemon");
-      }).catch(
-         function (err) {
-            res.send("DB create error.",err);
-      });
-   });
-
+         }).catch(
+            function (err) {
+               res.send("DB create error.",err);
+            });
+         });
 });
 
-//BONUS TODO: delete
+//--delete
 router.delete("/:id", function(req,res) {
    if(req.params.id) {
       db.pokemon.destroy({
@@ -58,7 +74,7 @@ router.delete("/:id", function(req,res) {
       }).then(
          function(deleted) {
             console.log("db delete",req.params.id);
-            res.send(success);
+            res.send("ok");
       }).catch(
          function(err) {
             res.send("DB error:"+err);
