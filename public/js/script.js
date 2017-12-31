6da8e6649adefef14506a294f10a3998561c62e9
peterpickles <lipeter2006@gmail.com>
2017-12-30 19:36:29 -0800

    working out rendering logic for pokemon specific page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
+
+//run this everytime I hit this crazy ass button
 $(".delete-pokemon").click(function(e){
 		e.preventDefault();
-		console.log("DELETEDÍ");
+		console.log("DELETED!");
 		$.ajax({
 			method: "DELETE",
 			url: $(this).attr("action"),
