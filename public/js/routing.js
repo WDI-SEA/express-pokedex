0bd9e24e71608f69606f755147e77dc6a02f468a
adamredwoods <adamredwoods@users.noreply.github.com>
2018-01-01 01:13:44 -0800

    cleanup

--- public/js/routing.js
+++ public/js/routing.js
@@ -2,17 +2,12 @@ $(".delete-button").click(function(e) {
    e.preventDefault();
    $(this).off("click");
    $("#spinner-"+e.target.id).removeClass("hidden");
-   
+
    $.ajax({
       url: $(this).attr("href"),
       method: "DELETE"
    }).success(function(data) {
-      //$(this).remove();
       window.location.href="/pokemon";
-      // $.ajax({
-      //    url: "/pokemon",
-      //    method: "GET"
-      // });
    }).error(function(err) {
       console.log("Ajax error.",err);
    });
