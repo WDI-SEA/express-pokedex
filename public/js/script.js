d890ab7896c0bed1257f39062cca041cc750c50c
Nick Nedev <nedevn@gmail.com>
2018-01-02 01:33:42 -0800

    Done

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,9 @@
 $('.delete-btn').click(function(e){
     e.preventDefault(); 
-    console.log($(this).attr('data-id'));
+    var url = 'pokemon/'+$(this).attr('data-id')
 
      $.ajax({
-        url: 'pokemon/'+$(this).attr('data-id'),
+        url: url,
         method: 'DELETE'
-     }).then(function(pokemon){
-        console.log(pokemon);
-     })
-})
+     });
+});
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,46 +5,46 @@ var request = require('request');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
+  // TODO: render favorites
   db.pokemon.findAll()
-    .then(function(pokemons){
-     // res.send(pokemons);
-     res.render('pokemon/favourites.ejs', {pokemons:pokemons});
+    .then(function(pokemons) {
+      // res.send(pokemons);
+      res.render('pokemon/favourites.ejs', { pokemons: pokemons });
     });
 
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    db.pokemon.create(req.body)
-    .catch(function(err){
+  // TODO: add to database
+  db.pokemon.create(req.body)
+    .catch(function(err) {
       res.status(500).render('error');
     });
-    
+     res.redirect('/pokemon');
 });
 
 // GET a specific pokemon and display more data about it. 
-router.get('/:id', function(req, res,next){
+router.get('/:id', function(req, res, next) {
   var detailsUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  
-  request(detailsUrl + req.params.id, function(err, response, body){
+
+  request(detailsUrl + req.params.id, function(err, response, body) {
     var pokemon = JSON.parse(body);
 
-    res.render('pokemon/singlePokeView', {pokemon:pokemon});
+    res.render('pokemon/singlePokeView', { pokemon: pokemon });
   });
 
 
 });
 
-
-router.delete('/:id', function(req, res){
-  console.log(req.params.id);
-  db.pokemon.destroy({where:{
-    pokemonId: req.params.id
-
-  }});
-
+router.delete('/:id', function(req, res, next) {
+res.redirect('/pokemon');
+  db.pokemon.destroy({
+    where: {
+      pokemonId: req.params.id
+    }  
+  });
+ 
 });
 
 module.exports = router;
