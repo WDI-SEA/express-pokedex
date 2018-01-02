7ee7b1f757b135824f2e5b53a16288d3cfeab6e8
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-01 21:24:49 -0800

    updated functions for flipping cards

--- public/js/card-flip.js
+++ public/js/card-flip.js
@@ -1,17 +1,23 @@
+var cardFlipped = function (){
+	var poke_id = $(this).attr('id');
+	var id = poke_id.substring(poke_id.length -1)
+	document.getElementById(poke_id).style.display = 'none';
+	document.getElementById('cardback-'+id+'').style.display = 'inline-block';
+}
+
 // Add Event Listeners to Cards
 var reflipCard = function (poke_id) {
-
-	document.getElementById('pokemon-'+poke_id+'').style.display = 'none';
-	document.getElementById('cardback-'+poke_id+'').style.display = 'inline';
+	var card_id = 'pokemon-'+poke_id
+	var card = document.getElementById(card_id)
+	card.addEventListener('click', cardFlipped);
 }
 
 // To Show Front of Card
 var cardClicked = function (){
-
 	var poke_id = $(this).attr('id');
 	document.getElementById('pokemon-'+poke_id+'').style.display = 'inline-block';
 	document.getElementById('cardback-'+poke_id+'').style.display = 'none';
-	//reflipCard(poke_id);
+	reflipCard(poke_id);
 
 }
 
