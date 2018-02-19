0dab8b3e165552b676824183f5397ef8092c7e75
danielvancura <danielvancura@gmail.com>
2018-02-18 16:20:59 -0800

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+// var rowdy = require('rowdy-logger');
 var app = express();
+
+// var rowdyResults = rowdy.begin(app)
+
+// ... apply your routes ...
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -10,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
 
     request(pokemonUrl, function(error, response, body) {
         var pokemon = JSON.parse(body).results;
@@ -20,6 +25,10 @@ app.get('/', function(req, res) {
 
 app.use('/pokemon', require('./routes/pokemon'));
 
-var server = app.listen(process.env.PORT || 3000);
+// var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, function() {
+    // rowdyResults.print();
+});
+
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(pokemon) {
+    res.render('index', { pokemon: pokemon });
+  });
 });
+
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
-});
+    db.pokemon.create(req.body).then(function(){
+    res.redirect('/pokemon');  
+    });
+
+  });
 
 module.exports = router;
