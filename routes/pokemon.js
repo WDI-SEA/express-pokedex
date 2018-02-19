e006f89760dc89d8228aeba1ffa348314a3bb628
hannan-emily <emilyhannan@gmail.com>
2018-02-19 15:49:25 -0800

    setting up routes to fill the show page with details on a clicked pokemon. now have to set up a button or link next to each pokemon on favorites page, so, a user can click any pokemon for more details (which will redirect to the show page)

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
-var router = express.Router();
+var request = require('request');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var app = express();
 
 // GET - return a page with favorited Pokemon
 // router.get('/', function(req, res) {
@@ -16,13 +19,25 @@ router.get('/', function(req, res) {
   });
 });
 
-  //grab a pokemon when a button is clicked on in index.ejs,
-  //the pokemon.name grabs info from the pokemon.js file (identifies WHICH pokemon is being grabbed
-  //we then grab all the info from that object here (req.body)
-  //and post it to the database pokemon with the db.pokemon.create call
-   //then we redirect to the pokemon favorites page
-  //select that body, which is connected to the API,
-  //then post that object to the databse, then redirect to the pokeomon page
+//GRAB ALL THE DETAILS OF EACH POKEMON - ID, MORE DETAILS NEEDED FOR SHOW/DETAILS PAGE
+router.get('/:id', function(req, res) {
+  db.pokemon.findById(req.params.id).then(function(pokemon) {
+    var thisPokemon = 'http://pokeapi.co/api/v2/pokemon/'+pokemon.name+'/'; //grab THIS pokemon at this URl
+    request(thisPokemon, function(error, response, body){
+      var pokemon = JSON.parse(body); //grab all the info from THIS pokemon's object array in that URL & parse
+      res.render('./poke/show', {pokemon:pokemon}); //render it all to the show page (pokemon data for thisPokemon)
+    }); //now we've  allowed all the info for THIS pokemon to be displayed on the show page
+  }); //need to link up the action that will call this (when we click on a individual pokemon for details)
+});
+
+
+//grab a pokemon when a button is clicked on in index.ejs,
+//the pokemon.name grabs info from the pokemon.js file (identifies WHICH pokemon is being grabbed
+//we then grab all the info from that object here (req.body)
+//and post it to the database pokemon with the db.pokemon.create call
+ //then we redirect to the pokemon favorites page
+//select that body, which is connected to the API,
+//then post that object to the databse, then redirect to the pokeomon page
 
 // ADD THAT POKEMON TO MY DATABASE OF FAVROTIES
 router.post('/', function(req, res) {
@@ -31,6 +46,15 @@ router.post('/', function(req, res) {
   });
 });
 
-//GET SPECIFIC POKEMON ID, AND REDIRECT TO THE SHOW PAGE?
+// base off cruddy board games project delete
+//DELETE POKEMON FROM DATABASE, WHICH WILL THEN DELETE FROM FAVORITE PAGE (IT DISPLAYS THE DB CONTENTS)
+router.delete('/:id', function(req, res) {
+  db.pokemon.destroy({
+    where: {id: req.params.id}
+  }).then(function(data){
+    res.redirect('favorites');
+  });
+});
+
 
 module.exports = router;
