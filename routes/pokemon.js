2bf4521ad84808b56e377ea0fabbb1afa2e5cbc1
Scott <scrosehart@gmail.com>
2018-02-20 02:36:38 -0800

    Scott Rosehart - pokedex

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var request = require('request');
+// var app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(data) {
+      res.render('favorites', { pokemon: data });
+  });
+});
+
+// //GET - specific pokemon from home page
+router.get('/:name', function(req, res) {
+  var pokeName = req.params.name;
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + pokeName;
+  request(pokemonUrl, function(error, response, body) {
+    var pokemon = JSON.parse(body);
+    res.render('show', { pokemon: pokemon });
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name,
+  }).then(function(data) {
+    res.redirect('/');
+  });
 });
 
+// DELETE - delete pokemon from favorites
+router.delete('/:name', function(req, res) {
+  db.pokemon.destroy({
+    where: {name: req.params.name}
+  }).then(function(data) {
+    res.send('');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+//
