94f888355d306c1292920d0aaa28297e6e21f365
Zakir Butte <zakir.butte@gmailcom>
2018-02-20 06:16:45 -0800

    added delete button and some stats

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,68 @@
+var db = require('../models');
 var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(data) {
+    console.log(data);
+    res.render('favorites', {pokemon: data});
+  });
+});
+
+
+
+router.get('/:id', function(req, res) {
+  db.pokemon.find({
+    where: {id: req.params.id}
+  }).then(function(data) {
+    var pokename = data.name;
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/'+ pokename;
+    console.log(pokemonUrl)
+    request(pokemonUrl, function(error, response, body) {
+        var pokemon = JSON.parse(body);
+        console.log(pokemon);
+        res.render('show', {pokemon: pokemon});
+      });
+    });
+  });
+
+  // router.get('/:name', function(req, res) {
+  //   db.pokemon.find({
+  //     where: {name: req.params.name}
+  //   }).then(function(data) {
+  //     var pokename = data.name;
+  //     var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/'+ pokename;
+  //     console.log(pokemonUrl)
+  //     request(pokemonUrl, function(error, response, body) {
+  //         var pokemon = JSON.parse(body);
+  //         console.log(pokemon);
+  //         res.render('show', {pokemon: pokemon});
+  //       });
+  //     });
+  //   });
+
+
+
+router.delete('/:name', function(req, res) {
+  db.pokemon.destroy({
+    where : {name: req.params.name}
+  }).then(function(data) {
+    console.log('DELETED ENTRY');
+    res.send('DELETED');
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {name: req.body.name}
+  }).then(function(data) {
+    console.log(data);
+    res.redirect('/pokemon');
+  });
 });
 
 module.exports = router;
