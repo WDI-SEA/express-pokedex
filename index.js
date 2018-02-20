fcb478c2a09a359f84de98adeae5123add9e1350
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-02-20 09:59:03 -0800

    Michael H pokedex

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var path = require('path');
 
-app.use(require('morgan')('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
@@ -14,12 +15,16 @@ app.get('/', function(req, res) {
 
     request(pokemonUrl, function(error, response, body) {
         var pokemon = JSON.parse(body).results;
+        //console.log(pokemon);
         res.render('index', { pokemon: pokemon });
     });
 });
 
+
+
 app.use('/pokemon', require('./routes/pokemon'));
 
 var server = app.listen(process.env.PORT || 3000);
+
 
 module.exports = server;
