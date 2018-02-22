3422e2cc96d8f7c81145d62ca0cc2842aae9a2b8
Daniel Heyward <danheyward@gmail.com>
2018-02-21 20:44:22 -0800

    functional, not styled. just wanted to catch up to the rest of class

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var request = require('request');
 
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-    // TODO: render favorites
-    res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(function(pokemon) {
+    res.render('pokemon', { pokemon: pokemon });
+  });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-    // TODO: add to database
-    res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(data) {
+    res.redirect('/pokemon');
+  });
+});
+
+router.get('/:id', function(req, res) {
+  db.pokemon.find({
+    where: { id: req.params.id }
+  }).then(function(data) {
+    request({
+      url: 'http://pokeapi.co/api/v2/pokemon/' + data.name
+    }, function(error, response, body) {
+      var dataObj = JSON.parse(body);
+      console.log(dataObj);
+      res.render('show', { pokemon: dataObj, name: req.params.id })
+    });
+  });
 });
 
 module.exports = router;
