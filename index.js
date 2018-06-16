01967abb3f7bdc779dc6e5173e3d11f830730b42
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-15 19:02:01 -0700

    initial commit with some routes added

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var path = require('path');
 var app = express();
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=50';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../models');
+
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(data) {
+  	// change index to a favorites page ejs!!!
+    res.render('pokemon/index', {pokemon: data});
+  });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(data) {
+    res.redirect('/pokemon');
+  });
+});
+
+// GET /pokemon/:id - get info about specific pokemon and display
+// router.get('/:id', function(req, res) {
+//   var id = parseInt(req.params.id);
+//     db.pokemon.find({
+//       where: {id: req.params.id}
+//     }).then(function(data) {
+//         res.render('show', {pokemon: data});
+//     });
+// });
+
+// DELETE - /pokemon/:id 
+router.delete("/:id", function(req, res) {
+  db.pokemon.destroy({
+    where: {id: req.params.id}
+  }).then(function(data) {
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
