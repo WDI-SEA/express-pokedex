6d66b2c0f7666f9e33b215fa100422cc09572f1c
shullmb <shullmb@gmail.com>
2018-06-16 10:11:54 -0700

    db connected, index route & vivew done

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then( function(pokemon) {
+    res.render('index', {pokemon});
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/:id', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then( function(pokemon) {
+    console.log(pokemon);
+    res.redirect('index');
+  })
 });
 
 module.exports = router;
