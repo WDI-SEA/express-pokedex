0bcd1a8c660885358a7695def208daf832d7a2ad
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-18 12:24:28 -0700

    Install dependencies, comments and minor ES6 syntaxing.

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+// All dependencies
 require('dotenv').config();
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const ejsLayouts = require('express-ejs-layouts');
 
+// Global var
+const app = express();
+
+// Sets and uses
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+// --- ROUTES ---
+app.use('/pokemon', require('./routes/pokemon'));
+
+
 // GET / - main index of site
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
-    res.render('index', { pokemon: pokemon });
+    res.render('index', { pokemon });
   });
 });
 
-// Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
-
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, () => {
+  console.log('You\'re listening to the smooth sounds of port 3000 in the morning');
+});
 
 module.exports = server;
