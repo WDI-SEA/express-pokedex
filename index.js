180a64c9e16b63decdb14b78150376f3209df292
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-19 14:13:53 -0700

    Favorites view and delete button working on Picachu ex. need add/post functionality

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+	console.log("getting pokemon");
+	db.pokemon.findAll().then(function(pokemon){
+		console.log('pokemon/index');
+		res.render('pokemon/index', {pokemon: pokemon});
+	}).catch(function(err){
+		console.log('oops', err); 
+		res.send('No DICE!');
+	});
 });
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
@@ -14,3 +22,16 @@ router.post('/', function(req, res) {
 });
 
 module.exports = router;
+
+router.delete("/:id", function(req, res){
+	console.log(req.params.id);
+	db.pokemon.destroy({
+		where: {id: req.params.id}
+	}).then(function(deletedPokemon){
+		console.log("deleted:", deletedPokemon);
+		res.send("successfully deleted");
+	}).catch(function(error){
+		console.log("error:", error);
+		res.send("did not delete");
+	});
+});
