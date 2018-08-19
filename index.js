9e0a29446cb9ab79b55161565bf7e9f978c61ee6
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-08-18 18:59:54 -0700

    added GET and POST functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 var express = require('express');
 var request = require('request');
@@ -10,15 +11,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+
+
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=60';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
     res.render('index', { pokemon: pokemon });
   });
 });
+
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,28 @@
+var db = require('../models');
 var express = require('express');
 var router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
+	console.log("catch 'em");
+	db.pokemon.findAll().then(function(pokemon){
+		console.log('pokemon/index');
+	res.render('pokemon/index', {pokemon: pokemon});
+	}).catch(function(err){
+		console.log('oops', err);
+		res.send('違うですよ！ ポケモン がいない');
+	});
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+
+});
+
+router.post('/', function(req, res){
+	db.pokemon.create(req.body).then(function(newPokemon){
+		console.log('caught pokemon:', newPokemon);
+		res.redirect('/pokemon');
+	}).catch(function(err){
+		res.send('見つけなかった...');
+	})
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
@@ -12,5 +30,7 @@ router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   res.send(req.body);
 });
+
+
 
 module.exports = router;
