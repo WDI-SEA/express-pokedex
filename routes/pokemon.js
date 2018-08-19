97685434880c6b3ce209706e749fdd66e54c435b
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-19 09:08:32 -0700

    create pokemon index placehodler

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  res.render('pokemon/index');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
@@ -13,4 +13,5 @@ router.post('/', function(req, res) {
   res.send(req.body);
 });
 
+/** @suppress {missingRequire} */
 module.exports = router;
