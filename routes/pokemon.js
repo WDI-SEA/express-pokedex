e77fcc08d3361400a2ee8e034f77deef00a89184
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-19 10:22:17 -0700

    finish get /pokemon route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,15 +6,25 @@ var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // get list of pokemon
+  // declare list of pokemon names to be sent
+  var nameList = [];
+  // get list of pokemon from database
   db.pokemon.findAll().then(function(pokemons) {
+    // console log entirety of fulfilled promise
     console.log('fulfilled promise from pokemon.findAll()', pokemons);
     pokemons.forEach(function(pokemon) {
+      // console log structure of data returned
       console.log('an ele in pokemons[]', pokemon);
       console.log('dataValues of ele:', pokemon.dataValues);
       console.log('name of pokemon:', pokemon.dataValues.name);
+      // add pokemon name to name list
+      nameList.push(pokemon.dataValues.name);
     });
-    res.render('pokemon/index');
+    // render pokemon index with all names sent for page to use
+    res.render('pokemon/index', { nameList: nameList });
+  }).catch(function(err) {
+    console.log('fetch index of pokemon from db failed with err', err);
+    res.send('awesome 404 page, enjoy it');
   });
 });
 
