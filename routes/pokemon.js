f051f9ebbab7a43a8e558e4d419d0380160230ff
DeusMalsith <metalgear132@gmail.com>
2018-08-19 21:03:56 -0700

    Favorites page is showing

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(pokemon) {
+  	console.log(pokemon);
+  	res.render('favorites', {pokemon:pokemon});
+  }).catch(function(err) {
+  	console.log('error');
+  	res.render('404');
+  });
+  // res.send('Render a page of favorites here');
+});
+
+router.get('/:id', function(req, res) {
+
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
