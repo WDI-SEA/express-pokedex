6682bc365308061090894444acd6b52bbf1ccdb2
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-20 08:26:10 -0700

    fix auto-running ajax on pokemon/index

--- public/js/ajax-routing.js
+++ public/js/ajax-routing.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
   console.log('ajax routing reached on this page');
 
   // get route and param
-  var url = $('#del-fav').attr('href');
 
-  $.ajax({
-    url: url,
-    method: 'DELETE'
-  }).done(function(data) {
-    console.log('finished unfavorite', data);
-  }).fail(function(err) {
-    console.log('failed to unfavorite with err', err);
+  var unfav = $('#del-fav').click(function(e) {
+    e.preventDefault();
+
+    var url = $('#del-fav').attr('href');
+
+    $.ajax({
+      url: url,
+      method: 'DELETE'
+    }).done(function(data) {
+      console.log('finished unfavorite', data);
+    }).fail(function(err) {
+      console.log('failed to unfavorite with err', err);
+    });
   });
 });
