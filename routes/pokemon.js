60a6a620dccd5eb223b69f52a2eb6d6050c54313
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-19 17:18:39 -0700

    add/delete buttons work. trying to add pokemon detail page for each clicked monster

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -15,23 +18,44 @@ router.get('/', function(req, res) {
 });
 
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+// Below is route to POST
+router.post('/', function(req, res){
+	db.pokemon.create(req.body).then(function(addPokemon){
+		console.log('added this pokemon:', addPokemon); 
+		res.redirect('/pokemon');
+	}).catch(function(err){
+		console.log('error happened', err); 
+		res.send('you dun goofed'); 
+	});
 });
 
-module.exports = router;
 
+router.get('/:id', function(req, res){
+	db.pokemon.findById(req.params.id).then(function(foundPokemon){
+		res.render('pokemon/show');
+	}).catch(function(err){
+		console.log('err', err);
+		res.render('404');
+	});
+});
+
+
+// Below is route for DELETE
 router.delete("/:id", function(req, res){
 	console.log(req.params.id);
 	db.pokemon.destroy({
 		where: {id: req.params.id}
-	}).then(function(deletedPokemon){
-		console.log("deleted:", deletedPokemon);
+	}).then(function(deletePokemon){
+		console.log("deleted:", deletePokemon);
 		res.send("successfully deleted");
 	}).catch(function(error){
 		console.log("error:", error);
 		res.send("did not delete");
 	});
 });
+
+
+
+
+module.exports = router;
+
