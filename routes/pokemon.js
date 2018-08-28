d511b935471c668fb5907914fccd442b5d41a5c2
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-28 09:59:18 -0700

    created more info page

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+var request = require('request');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -18,6 +19,16 @@ router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   db.pokemon.create(req.body).then(function(){
   	res.redirect('/pokemon');
+  });
+});
+
+router.get('/:id', function(req, res){
+	let pokeName = req.params.id;
+  var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokeName}`;
+  
+  request(pokemonUrl, function(error, response, body) {
+    let pokemon = JSON.parse(body);
+    res.render('pokemon/info', { name: pokemon.name, image: pokemon.sprites.front_shiny, exp: pokemon.base_experience, type: pokemon.types[0].type.name, abilities: pokemon.abilities, stats: pokemon.stats });
   });
 });
 
