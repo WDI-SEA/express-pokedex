fcd0a7e2af3a4cdd37ee2bc53a1b03917939bb6a
chidrome <chidrome@gmail.com>
2018-12-27 17:05:54 -0800

    pokemon.js file not liking var db delcaration

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./models');
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(function(foundPokemon){
+  	res.render('./views/results', { pokemons: foundPokemon })
+  })
+  .catch(function(error){
+  	console.log('Error Message', error);
+  	res.send('Error, check your logs');
+  })
 });
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
