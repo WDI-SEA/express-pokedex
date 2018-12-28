90e13d5ac9ffcee25e9b6220501bfffcded93b26
Nhuuu <nqtrinh87@gmail.com>
2018-12-28 14:28:11 -0800

    part 3 done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
+// Middleware, morgan - http request logger
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +13,7 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then((favePokemon) => {
+  	res.render('favorites', {favePokemon: favePokemon})
+  })
+  .catch((err) => {
+  	console.log('fave', err)
+  	res.send('Error, check your logs!')
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+  	where: {name: req.body.name}
+  	}).spread((pokemon, created) => {
+  	console.log(pokemon.get());
+  	console.log(created);
+  	res.redirect('/pokemon');
+  	})
 });
 
 module.exports = router;
