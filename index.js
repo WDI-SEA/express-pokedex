875bb852f9561d7c6e523542db054a5c1cd82423
Parker Couch <parkercouch@gmail.com>
2018-12-30 12:32:25 -0800

    Changing db schema and dependencies

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const ejsLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override')
 const app = express();
 
-// override with POST having ?_method=DELETE or ?_method=PUT
+// SET MIDDLEWARE
 app.use(methodOverride('_method'))
-
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
-  request(pokemonUrl, function(error, response, body) {
+  request(pokemonUrl, (error, response, body) => {
     var pokemon = JSON.parse(body).results;
     res.render('index', { pokemon: pokemon });
   });
