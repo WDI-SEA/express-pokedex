699ce6365840bbbe3ddb2b82e7b3e3540566a6dc
idothestamping <douglas.klemp@gmail.com>
2018-12-29 23:53:32 -0800

    initial commit: favorite and delete functional
    Detail button under construction

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var methodOverride = require('method-override');
 
+
+app.use(express.static(__dirname + '/public')); //for css
+
+app.use(methodOverride('_method'));
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +17,7 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
@@ -26,3 +31,5 @@ app.use('/pokemon', require('./routes/pokemon'));
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
+
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models'); //this needed to be added to reference my db
+var methodOverride = require('method-override');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+ 	 db.pokemon.findAll().then( function(favorite) {
+  		res.render('faves', {favorite: favorite})
+ 		}).catch( function(err) {
+  			console.log('faves', err)
+  			res.send('Error, check your logs!')
+  			})
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  	db.pokemon.findOrCreate({
+  		where: {name: req.body.name}
+  		}).spread( function(pokemon, created) {
+  			console.log(pokemon.get());
+  			res.redirect('/pokemon');
+  			})
+});
+
+
+router.get('/:id', (req, res) => {
+	var findName = db.pokemon.findById(parseInt(req.params.id))
+	.then((pokeName) => {
+		var resultUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151'+ pokeName.name.toLowerCase();
+		request(resultUrl, function (error, response, body) {
+			var pokemonData = JSON.parse(body);
+			res.render('show', {pokemon: pokemonData});
+      console.log(pokemonData.types[0].type.name)
+		})
+	});
+});
+
+
+router.delete('/', function(req, res) {
+	db.pokemon.destroy({
+  		where: {name: req.body.name}
+		}).then( function(pokemon, deleted) {
+  // do something when done deleting
+  		res.redirect('/pokemon');
+  	})
 });
 
 module.exports = router;
