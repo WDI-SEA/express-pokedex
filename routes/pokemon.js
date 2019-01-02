8748fde7d29257dad2671a83c265fda62bfff5ae
brandiw <brandimichelle.0911@gmail.com>
2019-01-02 12:07:08 -0800

    Adding basic solution

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var request = require('request');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  // Get all records from the DB and render to view
+  db.pokemon.findAll()
+  .then(function(foundPokemons){
+    res.render('favorites', { pokemon: foundPokemons });
+  }).catch(function(err){
+    console.log('error', err);
+    res.render('error');
+  });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  })
+  .spread(function(poke, wasCreated){
+    res.redirect('/pokemon');
+  })
+  .catch(function(err){
+    console.log('error', err);
+    res.render('error');
+  });
+});
+
+// GET /pokemon/3
+router.get('/:name', function(req, res){
+  if(req.params && req.params.name){
+    request('https://pokeapi.co/api/v2/pokemon/' + req.params.name, function(error, response, body){
+      if(error || response.statusCode != 200){
+        res.render('error');
+      }
+      else {
+        var pokedata = JSON.parse(body);
+        res.render('show', { pokedata: pokedata });
+      }
+    });
+  }
+  else {
+    res.render('error');
+  }
 });
 
 module.exports = router;
