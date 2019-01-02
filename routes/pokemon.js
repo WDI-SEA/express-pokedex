888cdb244095b4b2e66593e7204e5defa7b0686d
idothestamping <douglas.klemp@gmail.com>
2019-01-02 09:05:15 -0800

    Detail working, page needs to be styled to match the main theme

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var db = require('../models'); //this needed to be added to reference my db
 var methodOverride = require('method-override');
@@ -26,16 +27,12 @@ router.post('/', function(req, res) {
 });
 
 
-router.get('/:id', (req, res) => {
-	var findName = db.pokemon.findById(parseInt(req.params.id))
-	.then((pokeName) => {
-		var resultUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151'+ pokeName.name.toLowerCase();
-		request(resultUrl, function (error, response, body) {
-			var pokemonData = JSON.parse(body);
-			res.render('show', {pokemon: pokemonData});
-      console.log(pokemonData.types[0].type.name)
-		})
-	});
+router.get('/:name', function(req, res) {
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + req.params.name;
+  request(pokemonUrl, function(error, response, body) {
+    var pokemonDetail = JSON.parse(body);
+    res.render('show', { pokemon: pokemonDetail });
+  });
 });
 
 
