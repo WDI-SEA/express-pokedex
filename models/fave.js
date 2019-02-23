cc3169dcabaeb0de44ab424ce56b9a3b8464972b
Carlo Bruno <carlofbruno@gmail.com>
2019-02-23 14:28:07 -0800

    created database and model

---
+++ models/fave.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const fave = sequelize.define('fave', {
+    name: DataTypes.STRING
+  }, {});
+  fave.associate = function(models) {
+    // associations can be defined here
+  };
+  return fave;
+};
