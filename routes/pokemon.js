67d8444458f1d4ad55df1764a98b2a55060e8fd1
Garrett Moore <robertgarrettmoore@gmail.com>
2019-02-24 07:34:30 -0800

    first commit. basic routes aside from GET ONE finished

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var request = require('request');
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(pokemon){
+    // res.send('FAVORITES');
+    res.render('favorites', {pokemon});
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(pokemon){
+    res.redirect('pokemon');
+  })
 });
+
+
+// GET ONE
+router.get('/:id', function(req, res){
+  let name = req.params.id;
+  let url = 'http://pokeapi.co/api/v2/pokemon/' + name + '/';
+  console.log(name);
+  request(url, function(error, response, body) {
+    let name = JSON.parse(body).name;
+    let image = JSON.parse(body).sprites.front_default;
+    let weight = JSON.parse(body).weight;
+    let height = JSON.parse(body).height;
+    let moves = JSON.parse(body).moves;
+    let abilities = JSON.parse(body).abilities;
+    db.pokemon.findById(parseInt(req.params.id)).then(function(pokemon) {
+      res.render('show', {pokemon, name, image, weight, height, moves, abilities});
+  })
+  });
+});
+
 
 module.exports = router;
