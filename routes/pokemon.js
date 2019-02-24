7914edf8f93167f761b1c9a209f9f46ddb11ff34
Garrett Moore <robertgarrettmoore@gmail.com>
2019-02-24 12:26:23 -0800

    finished routes, added delete functionality and css styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 var request = require('request');
+var methodOverride = require('method-override');
 
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   db.pokemon.findAll().then(function(pokemon){
-    // res.send('FAVORITES');
     res.render('favorites', {pokemon});
   })
 });
@@ -23,22 +23,29 @@ router.post('/', function(req, res) {
 
 
 // GET ONE
-router.get('/:id', function(req, res){
-  let name = req.params.id;
+router.get('/:id', function(req,res) {
+  db.pokemon.findById(req.params.id).then(function(pokemon) {
+  let name = pokemon.name;
   let url = 'http://pokeapi.co/api/v2/pokemon/' + name + '/';
   console.log(name);
-  request(url, function(error, response, body) {
-    let name = JSON.parse(body).name;
+  request(url, function (error, response, body) {
     let image = JSON.parse(body).sprites.front_default;
     let weight = JSON.parse(body).weight;
     let height = JSON.parse(body).height;
     let moves = JSON.parse(body).moves;
     let abilities = JSON.parse(body).abilities;
-    db.pokemon.findById(parseInt(req.params.id)).then(function(pokemon) {
-      res.render('show', {pokemon, name, image, weight, height, moves, abilities});
-  })
+  res.render('show', {name, image, weight, height, moves, abilities} );
+  });
   });
 });
 
+// DELETE ONE
+router.delete('/:id', function(req, res) {
+  db.pokemon.destroy({
+    where: {id: req.params.id}
+  }).then(function() {
+    res.redirect('/pokemon');
+  })
+})
 
 module.exports = router;
