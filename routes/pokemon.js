fb23d8b9cd3bea6d7c4199b73e9ff010301156ca
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-02-25 10:53:23 -0800

    edits

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -15,22 +15,15 @@ router.get('/', function(req, res) {
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   db.pokemon.create({
-    name: req.body.name
+    name: req.body.name //want to update name in db and its values
   }).then(function() {
-    res.redirect('pokemon')
+    res.redirect('pokemon') //this will go into  the above get route
   })
   // TODO: Get form data and add a new record to DB
   
 });
 
-// GET ONE POKEMON
-// router.get('/:id', function(req, res) {
-//   db.pokemon.findById(req.params.id).then( function(pokemon) {
-//     // res.json(pokemon)
-//     res.render('show', {pokemon})
-//   })
-// })
-
+//SHOW ONE POKEMON
 router.get('/:name', function(req, res) {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + req.params.name;
     request(pokemonUrl, function(error, response, body) {
@@ -39,13 +32,6 @@ router.get('/:name', function(req, res) {
     });
 });
 
-// // UPDATE ONE POKEMON
-// router.put('/:id', function(req, res) {
-//   db.pokemon.update({
-//     name: req.body.name
-//   }, { where: {id: req.params.id}}).then( function() {
-//     res.redirect('pokemon/' + req.params.id)
-//   })
-// })
 
 module.exports = router;
+//anything attached to router gets exported
