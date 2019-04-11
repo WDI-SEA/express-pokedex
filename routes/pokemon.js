f89743808bfcb04598a772283d172e041f072a78
Hannah Erickson <hannaherickson@Hannahs-MacBook-Air.local>
2019-04-10 18:46:22 -0700

    submit

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
+var request = require('request');
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function (pokemons) {
+  res.render('pokemon/faves', {pokemons})
+  });
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post('/', function(req, res){
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(){
+    res.redirect('pokemon')
+  });
 });
+
+router.get('/:id', function(req, res){
+  db.pokemon.findById(req.params.id).then(function(pokemon){
+    request('http://pokeapi.co/api/v2/pokemon/' + pokemon.name , function(error, response, body) {
+      var info = JSON.parse(body)
+      // res.json(info)
+      res.render('pokemon/show', {info});
+    })
+  });
+});
+
+router.delete('/:id', function(req, res){
+  db.pokemon.destroy({
+    where: {id:req.params.id}
+  }).then(function(){
+    res.redirect('/pokemon')
+  })
+});
+// TODO: Get all records from the DB and render to view
+//POST /pokemon - receive the name of a pokemon and add it to the database
+// TODO: Get form data and add a new record to DB
+//sometimes do not use full crud routes
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 require('dotenv').config();
 var express = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+const methodOverride = require('method-override');
 
-app.use(require('morgan')('dev'));
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('publc'));
+app.use(methodOverride('_method'));
 app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render('index', { pokemon });
   });
 });
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
+
 
 var server = app.listen(process.env.PORT || 3000);
 
