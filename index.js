1f12d133a128d02ce9a94a1ced5239ae524bda59
Sarah King <009kings@gmail.com>
2019-04-12 15:38:48 -0700

    don't need no body parse

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 var express = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 // GET / - main index of site
