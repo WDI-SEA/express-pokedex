8172786e0377b4bfcd995390613eb6f481373f7e
mtom92 <torresmoranm@gmail.com>
2019-04-13 20:34:11 -0700

    all working

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then((poke)=>{
+    res.render('show',{ poke})
+  })
+  .catch((err)=>{
+    console.log('err',err)
+    res.render('404')
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+
+
+router.post('/',(req,res)=>{
+
+  db.pokemon.create(req.body)
+  .then(()=>{
+    res.redirect('/pokemon')
+  })
+  .catch((err)=>{
+    console.log('error',err)
+    res.render('404')
+  })
 });
 
 module.exports = router;
