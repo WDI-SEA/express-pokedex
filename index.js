c5e1d693f851a151f8f70b2d9619ffcfd5a44de1
onoroff <adam.grunke@gmail.com>
2019-06-23 13:35:01 -0700

    initial commit

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(require('morgan')('dev'));
+// app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     var pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render('index', { pokemon });
   })
 });
 
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,62 @@
 var express = require('express');
 var router = express.Router();
+// notes from Steve
+var db = require('../models'); // have to go up a directory and then down into models
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  // call the db.pokemon.findAll
+  // Get data and render it into an ejs
+  db.pokemon.findAll().then(function(data){
+    res.render('pokemon', {data});
+    // res.json(pokemon);
+  });
+
+  // res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  // call db.pokemon.create and pass in the dat a from the form
+  // When the promise returns, we need to redirect to /pokemon
+  db.pokemon.create({
+    name: req.body.name
+  }).then(function(data){
+    res.render('pokemon', {data});
+
+  })
+
+
+  // res.send(req.body);
 });
+
+// GET /pokemon id. Gets one pokemon id from the database and uses it to look up details
+// about that one pokemon. 
+router.get('/:id', function(req, res) {
+  // look up pokemon in our db by its ID. HINT: (findByPk)
+  // use the pokemon name from the db to query the api for 
+  // details on that one pokemon 
+  // Take data from the api and render a detal/show page for this one pokemon. 'show' or 'details' page.
+  // res.send('This is the route for showing one pokemon');
+  db.pokemon.findOne({
+    where: {id: parseInt(req.params.id)}
+    }).then(function(data) {
+    res.json(data);
+  });
+});
+
+// Delete ONE record route
+router.delete('/:id', function(req, res) {
+  db.pokemon.destroy({
+      where: {id: parseInt(req.params.id)}
+  }).then(function(data) {
+      res.json(data);
+  });
+});
+
+// PUT /pokemon/:id
+// router.put('/:id')
 
 module.exports = router;
