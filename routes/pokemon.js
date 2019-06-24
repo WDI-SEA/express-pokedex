fdf32e0f6bc71b507d0313057b19574df99e4f93
onoroff <adam.grunke@gmail.com>
2019-06-23 18:18:36 -0700

    show details when manually nav to page. prevent repeat adds to fav

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios'); 
-
 // notes from Steve
 var db = require('../models'); // have to go up a directory and then down into models
 
@@ -11,6 +10,9 @@ router.get('/', function(req, res) {
   // call the db.pokemon.findAll
   // Get data and render it into an ejs
   db.pokemon.findAll().then(function(data){
+    
+   
+    
     res.render('pokemon', {data});
     // res.json(pokemon);
   });
@@ -22,13 +24,22 @@ router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   // call db.pokemon.create and pass in the dat a from the form
   // When the promise returns, we need to redirect to /pokemon
-  db.pokemon.create({
-    name: req.body.name
-  }).then(function(data){
-    // res.render('pokemon', {data});
+  
+  db.pokemon.findAll().then(function(data){
+    let repeatCount = 0;
+    data.forEach(function(pokemon){
+      if (req.body.name === pokemon.name){
+        repeatCount++;
+      }
+    })
+    if (repeatCount === 0) {
+      db.pokemon.create({
+        name: req.body.name
+      }).then(function(data){
+      })
+    }    
     res.redirect('pokemon');
-  })
-  // res.send(req.body);
+  });
 });
 
 // GET /pokemon id. Gets one pokemon id from the database and uses it to look up details
@@ -39,32 +50,16 @@ router.get('/:id', function(req, res) {
   // details on that one pokemon 
   // Take data from the api and render a detal/show page for this one pokemon. 'show' or 'details' page.
   // res.send('This is the route for showing one pokemon');
-  
-  
-  
+  var id = req.params.id;
 
-
-      var pokemonDetailsName = 'charizard';
-      var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokemonDetailsName}`;
-      // Use request to call the API
-      axios.get(pokemonUrl).then( function(apiResponse) {
-        var pokemon = apiResponse.data;
-
-        // res.render('show', { pokemon });
-        console.log(pokemon.abilities[0].ability.name)
-        res.render('show');
-      })
-
-
-  
-  
-  
-  
-  // db.pokemon.findOne({
-  //   where: {id: parseInt(req.params.id)}
-  //   }).then(function(data) {
-  //   res.json(data);
-  // });
+  db.pokemon.findByPk(id).then(function(pokemon) {
+    var pokemonName = pokemon.name  
+    var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    axios.get(pokemonUrl).then( function(apiResponse) {
+      var pokemon = apiResponse.data;
+      res.render('show', { pokemon });
+    })   
+  })
 });
 
 // Delete ONE record route
