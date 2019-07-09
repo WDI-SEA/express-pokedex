a248fff2295703efff1052994c75811a89575409
Danh Vuong <danhvuong@Danhs-Macbook.local>
2019-07-09 06:00:27 -0700

    Finale

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();// use .env file, need to install and touch .env 
+const express = require('express'); // for express framework, need npm i express and call app=express()
+const axios = require('axios'); // node module, for retrieving API , need install b4 use
+const ejsLayouts = require('express-ejs-layouts'); // for layout, need install b4 use
+const app = express(); // calling express to use
+const port = process.env.PORT || 3000; // setting port 
+const methodOverride =require('method-override'); // for method override , need install 
+
 
 app.use(require('morgan')('dev'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
-app.use(ejsLayouts);
+app.set('view engine', 'ejs'); // to use ejs files
+app.use(express.urlencoded({ extended: false })); //middle ware, to recognize the incoming Request Object as strings or arrays
+app.use(express.static(__dirname + '/public')); // to use public folder, like css , js , img ..etc..
+app.use(ejsLayouts); // use ejsLayouts
+app.use(methodOverride('_method')); //use methodOverride
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     var pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    var pokeUrl = pokemon.url;
+    // var pokemonImg;
+    // pokemon.forEach(function(singlePoke){
+    //   axios.get(pokeUrl).then(function(apiResponse){
+    //     pokemonImg = apiResponse.data.results;
+    //   })
+    // })
+    res.render('index', {pokemon});
   })
 });
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
+
+
 
 var server = app.listen(port, function() {
   console.log('...listening on', port );
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+const axios = require('axios');
+const methodOverride = require('method-override');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+
+  // call db.pokemon.findAll
+  db.pokemon.findAll().then(function(pokes){
+    res.render('pokemon/favPokemons', {pokes})
+  })
+  // Get data and render it into an ejs
+  // res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  // call db.pokemon.create and pass in the data from the form
+
+db.pokemon.create({
+    name: req.body.name
+  }).then(function(poke){
+    res.json(poke)
+  })
+  res.redirect('/pokemon')
+  // When the promise returns, we need to redirect to /pokemon
 });
 
+// GET /pokemon/:id - Gets one pokemon id from the database
+// and uses it to look up details about that one pokemon
+router.get('/:id', function(req, res){
+  // Look up pokemon in our db by its ID (findBypk)
+  let id = parseInt(req.params.id);
+  db.pokemon.findByPk(id).then(function(poke){
+    var pokeName = poke.name;
+    var pokeUrl = "http://pokeapi.co/api/v2/pokemon/" + pokeName + "/";
+    axios.get(pokeUrl).then( function(apiResponse){
+      let pokeDetails = apiResponse.data;
+      res.render('pokemon/pokeDetails', {pokeDetails, id})
+    })
+  })
+  // Use the pokemon name from the db to query the api for details on that one pokemon
+  // Take data from the API and render a detail/show page for this one pokemon
+  
+})
+
+router.delete('/:id', function(req, res){
+  let id = parseInt(req.params.id);
+  db.pokemon.destroy({
+    where: {id: id}
+  }).then(function(pokemons){
+    res.redirect('/pokemon');
+  })
+  
+})
 module.exports = router;
