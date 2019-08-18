168964a65b8862bd43ab5e2b73887aec8d6897fb
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2019-08-18 15:06:07 -0700

    installations and connect form to database

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const db = require('./models');
+const bodyParser = require('body-parser')
+const path = require('path');
 const port = process.env.PORT || 3000;
+
+// db.pokemon.findOne().then(function(pokemon){
+//   console.log(pokemon);
+// })
+
+// db.pokemon.findOne({
+//   where: {
+//     name: 'Pikachu'
+//   }
+// })
+// .then(function(pokemon){
+// console.log(pokemon);
+// })
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use('/pokemon', require('./routes/pokemon.js'))
 
 // GET / - main index of site
 app.get('/', function(req, res) {
@@ -26,5 +45,6 @@ app.use('/pokemon', require('./routes/pokemon'));
 var server = app.listen(port, function() {
   console.log('...listening on', port );
 });
+
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+
+
+db.pokemon.findAll()
+.then(function(pokemon) {
+  console.log('Found: ', pokemon.name)
+})
+  res.render('./favorites.ejs', {
+    name: name
+  });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  
+db.pokemon.create(req.body).then(function(poke) {
+  console.log('Created: ', poke.name)
+})
+  res.redirect('/pokemon');
 });
 
 module.exports = router;
+
+
