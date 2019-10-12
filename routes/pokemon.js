f28f2ba8792348fba9d3ecbaf0c6a9e14708a807
Brandon205 <brandonblack02@gmail.com>
2019-10-12 16:16:37 -0700

    Working w/out styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -12,9 +12,14 @@ router.get('/', function(req, res) {
   })
 });
 
-router.get('/pokemon/:id', function(req, res) {
-  
-})
+router.get('/:id', function(req, res) {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.id}`)
+  .then(function(foundPokemon) {
+    // res.send(foundPokemon);
+    res.render('show.ejs', {pokemon: foundPokemon.data});
+    })
+    // .catch(err=>console.log(err));
+  })
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
