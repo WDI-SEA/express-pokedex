9d04d426e4c019398b5e0cf57f825fe708116b71
Brandon205 <brandonblack02@gmail.com>
2019-10-13 09:51:17 -0700

    More sytling and minor changes to info displayed

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,7 +5,6 @@ const db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
   db.pokemon.findAll() 
   .then(function(favoritedPokemon) {
     res.render('faves.ejs', { pokemon: favoritedPokemon });
@@ -15,25 +14,30 @@ router.get('/', function(req, res) {
 router.get('/:id', function(req, res) {
   axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.id}`)
   .then(function(foundPokemon) {
-    // res.send(foundPokemon);
     res.render('show.ejs', {pokemon: foundPokemon.data});
     })
-    // .catch(err=>console.log(err));
   })
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
   db.pokemon.findOrCreate({
     where: {
       name: req.body.name
     }
   })
   .then(function([pokemon, created]) {
-    console.log(`${pokemon} was ${created ? 'created' : 'found'}`);
-    res.redirect('/pokemon');
+    res.redirect('/');
   })
-  // res.send(req.body);
 });
+
+router.delete('/:id', function(req, res) {
+  db.pokemon.destroy({
+    where: {
+      name: req.params.id,
+    }
+  })
+    .then(function(deletedPokemon) {
+      res.redirect('/pokemon');
+    })
+})
 
 module.exports = router;
