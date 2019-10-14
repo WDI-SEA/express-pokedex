3141b12ecbebd2761068a24694b1e49a00dea80f
sross9845 <sean.ross.985@gmail.com>
2019-10-14 08:54:45 -0700

    finished delete button and slight styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 const axios = require('axios'); 
+const methodOverride = require('method-override');
+router.use(methodOverride('_method'));
+
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
   db.pokemon.findAll().then(function(faves) {
-	  res.render('faves', { pokemon: faves })
+	res.render('faves', { pokemon: faves })
 	})
 });
 
@@ -14,21 +16,31 @@ router.get('/', function(req, res) {
 router.get('/:id', function(req,res){
   db.pokemon.findByPk(req.params.id)
   .then(function(result){
-    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + result.dataValues.name + '/';
+    var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/' + result.name + '/';
     axios.get(pokemonUrl).then( function(response) {
-    res.render('show', {result: result, pokemon: response.data})
+    res.render('show', {pokemon: response.data})
   })
 })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
   db.pokemon.findOrCreate({
 	  where: { name: req.body.name }
 	}).then(function(fave) {
 		res.redirect('pokemon')
 	})
 });
+
+router.delete('/:id', function(req,res) {
+	db.pokemon.destroy({
+		where: {
+      name: req.params.id
+    }
+	})
+	.then(function(pokemon) {
+		res.redirect('/pokemon')
+	})
+})
 
 module.exports = router;
