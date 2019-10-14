75da67475599fe038a93f073789752332e1973ae
Clay Darland <clay@darland.info>
2019-10-13 18:44:22 -0700

    Project's is working. Scaffolding is up and site design is finished. Need to tune SQL a

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const BASE_POKE_URL = 'http://pokeapi.co/api/v2/pokemon/';
+const BASE_SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species/'
 
-// GET /pokemon - return a page with favorited Pokemon
+// GET / - main index of site
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  // Use request to call the API
+  axios.get(BASE_URL).then( function(apiResponse) {
+    var pokemon = apiResponse.data.results;
+    res.render('index', { 
+      pokemon: pokemon.slice(0, 151),
+      pokeID: 0
+    });
+  })
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+// //Build router for pokemon detail page
+router.get('/pokemon/:id', function(req, res) {
+  axios.all([
+    axios.get(BASE_POKE_URL + req.params.id),
+    axios.get(BASE_SPECIES_URL + req.params.id)
+  ])
+  .then(axios.spread(function (deetsRes, speciesRes) {
+    res.render('details', {
+      deets: deetsRes.data,
+      species: speciesRes.data
+    })
+  }))
+})
 
 module.exports = router;
+
