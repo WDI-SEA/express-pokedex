1dfaab51e56795baf1b61b91c4ccea4b07fc1b35
bjgoddard <bjgoddard94@gmail.com>
2019-12-14 16:48:50 -0800

    at this exact moment everything works

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -12,7 +14,7 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     var pokemon = apiResponse.data.results;
@@ -20,8 +22,11 @@ app.get('/', function(req, res) {
   })
 });
 
+
+
 // Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+app.use('/', require('./routes/pokemon'));
+
 
 var server = app.listen(port, function() {
   console.log('...listening on', port );
