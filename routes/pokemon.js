412f105029822321fa5172f1044d335ab404d535
yboodhan <yboodhan@gmail.com>
2019-12-15 18:38:16 -0800

    added favorites functionality, basic

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(function(pokemon) {
+    res.render('index', { pokemon });
+  });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create(req.body)
+    .then(function(poke) {
+    res.redirect('/pokemon');
+  });
 });
 
 module.exports = router;
