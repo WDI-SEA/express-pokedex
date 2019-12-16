3d8d1e5b0f4a2b91350318e802cd97dd4174ea50
yboodhan <yboodhan@gmail.com>
2019-12-15 17:50:51 -0800

    commenting file and adding console log to understand code

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-require('dotenv').config();
+require('dotenv').config(); //loads environment variables
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios'); //HTTP client
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //where to listen
 
-app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev')); //HTTP request logger
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //body parser
 app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
-  axios.get(pokemonUrl).then( function(apiResponse) {
+  axios.get(pokemonUrl)
+    .then( function(apiResponse) {
+      console.log(apiResponse.data.results);
     var pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
@@ -28,3 +30,4 @@ var server = app.listen(port, function() {
 });
 
 module.exports = server;
+ 
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -14,3 +14,4 @@ router.post('/', function(req, res) {
 });
 
 module.exports = router;
+ 
