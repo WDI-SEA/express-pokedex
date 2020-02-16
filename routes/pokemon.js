09cc12f4545125ff56f7d63620df1246153066d7
melissay94 <melissay94@yahoo.com>
2020-02-16 11:37:46 -0800

    Set up database information

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,30 @@
 var express = require('express');
+const db = require("./")
 var router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(pokemon => {
+      res.json(pokemon);
+    }).catch(err => {
+      res.send(`Error: ${err}`);
+    });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      pokemon: req.body.name
+    }
+  }).then(([pokemon, created]) => {
+    res.redirect(`/pokemon/${pokemon.id}`);
+  }).catch(err => {
+    res.send(`Error: ${err}`);
+  });
 });
 
 module.exports = router;
