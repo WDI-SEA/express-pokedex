b03f952d8ce92794d6e290a9a3b77cc4d185b5d2
tinnerrss <tinnerrss@hotmail.com>
2020-02-17 17:34:07 -0800

    ughhhhh

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../../models')
+const axios = require("axios");
+
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(pokemons) {
+    // res.json(pokemons);
+    // console.log(pokemons[0])
+    res.render('favorites', {pokemon: pokemons});
+
+  }).catch(err => {
+    console.log(err)
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name,
+      url: req.body.url
+    }
+  }).then(function([pokemon, created]) {
+    res.redirect(`/pokemon`);
+  }).catch(err => {
+    console.log(err)
+    res.send("ERROR")
+  })
 });
+
+router.get("/:name", (req, res) => {
+  db.pokemon.findOne({
+    where: {
+      name: req.params.name
+    }
+  }).then(function(pokemon) {
+    axios.get(pokemon.url)
+    .then(function(response) {
+      console.log(response.data.types);
+      onePokemon = {
+        name: response.data.name,
+        type: response.data.types,
+        url: response.data.species.url
+      }
+      res.render('show', { onePokemon })
+
+    })
+    
+  })
+})
 
 module.exports = router;
