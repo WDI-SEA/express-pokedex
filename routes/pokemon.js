183fa0e77f0663b2e0586abf68a7af4158981cdd
dlew253 <dlew253@yahoo.com>
2020-02-18 09:02:30 -0800

    yeah its not working

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var router = express.Router();
+const db = require('../models');
+
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll() 
+  .then(function(favoritePokemon) {
+    res.render('faves.ejs', { pokemon: favoritePokemon });
+  })
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
+router.get('/:id', function(req, res) {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.id}`)
+  .then(function(foundPokemon) {
+    res.render('show.ejs', {pokemon: foundPokemon.data});
+    })
+  })
+
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  })
+  .then(function([pokemon, created]) {
+    res.redirect('/');
+  })
 });
+
+router.delete('/:id', function(req, res) {
+  db.pokemon.destroy({
+    where: {
+      name: req.params.id,
+    }
+  })
+    .then(function(deletedPokemon) {
+      res.redirect('/pokemon');
+    })
+})
 
 module.exports = router;
