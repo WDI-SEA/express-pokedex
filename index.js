3d689c8f0d1b9a0bb474b6fda86ef40f241892ba
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-04-18 17:21:22 -0700

    faves and delete faves

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 || 5000;
+let methodOverride = require('method-override');
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(methodOverride('_method')) 
+// Imports all routes from the pokemon routes file
+app.use('/pokemon', require('./routes/pokemon'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
@@ -21,12 +25,13 @@ app.get('/', function(req, res) {
   })
 });
 
-// Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+
+
 
 var server = app.listen(port, function() {
   console.log('...listening on', port );
 });
 
 module.exports = server;
+
 
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios'); 
 let db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
@@ -38,5 +39,47 @@ router.post('/', function(req, res) {
   })
   
 });
+
+router.get('/:id',(req,res)=>{
+    let id = req.params.id;
+    db.pokemon.findOne({
+      where: {id: id},
+    })
+    .then((pokemon)=>{
+        console.log('id',id)
+        var pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/'+ pokemon.name+'/';
+        //https://pokeapi.co/api/v2/pokemon/1/
+        // Use request to call the API
+        console.log('url',pokemonUrl)
+        axios.get(pokemonUrl).then( function(apiResponse) {
+          var pokemonApi = apiResponse.data;
+          console.log('pokemonApi abilities',pokemonApi.abilities[0].ability.name)
+          console.log('image url',pokemonApi.sprites.front_default)
+          res.render('show', { pokemon: pokemonApi, name:pokemon.name});
+        })
+        .catch((err)=>{
+          console.log('Error',err)
+          res.render('error')
+        })
+    })
+    .catch((err)=>{
+      console.log('Error',err)
+      res.render('error')
+    })
+})
+
+router.delete('/:id',(req,res)=>{
+  db.pokemon.destroy({
+    where: {id: req.params.id}
+  })
+  .then(()=>{
+    res.redirect('/pokemon')
+  })
+  .catch(err=>{
+    console.log('error in delete',err)
+    res.render('error')
+  })
+ 
+})
 
 module.exports = router;
