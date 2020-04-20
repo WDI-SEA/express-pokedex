b3099d043744131bf304d00b156e69504375ef08
ahonore42 <a1honore80@gmail.com>
2020-04-20 02:13:02 -0700

    Functional pokedex, but ohhh laaawd that's a lot of data and console logs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,11 +12,32 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=807';
   // Use request to call the API
-  axios.get(pokemonUrl).then( function(apiResponse) {
+  axios.get(pokemonUrl)
+  .then( function(apiResponse) {
+    //console.log(axios.get(apiResponse.data))
     var pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    // console.log(pokemon)
+    pokeData = pokemon.map(value => value.url);
+    // console.log(pokeData)
+    //pokeInfo = [];
+    // pokeData.forEach((url) => {
+    //   axios.get(url).then(response => {
+    //     // do something with response
+    //     console.log(response.data)
+    //     pokeInfo.push(response.data);
+    //   })
+    // })
+    // axios.get(pokeData)
+    // .then((apiTwo) => {
+    //   let data = apiTwo.data
+      // console.log(data)
+      res.render('index', { 
+        pokemon: pokemon.slice(0, 251)
+        
+      });
+    // })
   })
 });
 
@@ -26,5 +47,6 @@ app.use('/pokemon', require('./routes/pokemon'));
 var server = app.listen(port, function() {
   console.log('...listening on', port );
 });
+
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models')
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // Get all records from the DB and render to view
-  db.pokemon.findAll({
-    name: req.body.name
-  })
+  db.pokemon.findAll()
   .then((poke) => {
     res.render('pokemon/index', { poke } );
   })
@@ -37,7 +36,34 @@ router.get('/:id', (req, res) => {
       where: { id: req.params.id },
     })
     .then((poke) => {
-      res.render('pokemon/show', {poke})
+      let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+      axios.get((pokemonUrl)+poke.name)
+      .then((apiResponse) => {
+        let data = apiResponse.data
+        let pokeId = data.id
+        let pokeMoves = data.moves
+        console.log(pokeMoves)
+        axios.get(('https://pokeapi.co/api/v2/pokemon-species/')+pokeId)
+        .then((apiTwo) => {
+          let species = apiTwo.data
+          //console.log(apiTwo)
+          //console.log('This is the pokemon id', pokeId)
+          moveData = [];
+          pokeMoves.forEach((dataMove) => {
+            //console.log(dataMove.move.url)
+            axios.get(dataMove.move.url).then(response => {
+              console.log(response.data)
+              moveData.push(response.data)
+            })
+          })
+          // axios.get(('https://pokeapi.co/api/v2/move/')+pokeMoves)
+          // .then((apiThree) => {
+          //   let moves = apiThree.data
+          //   console.log(moves)
+          // })
+          res.render('pokemon/show', {poke, data, species, moveData})
+        })
+      })
     })
     .catch((err) => {
       console.log(err)
