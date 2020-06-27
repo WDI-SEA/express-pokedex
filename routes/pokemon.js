f51d0826e14c9c2ba4e76f4ccd4dee29d07c72f6
Eryoung-610 <eryoung610@gmail.com>
2020-06-26 19:36:42 -0700

    Display favorites added

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,24 @@
 var express = require('express');
+const { render } = require('ejs');
 var router = express.Router();
+let db = require('../models');
+const { default: Axios } = require('axios');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(pokemons => {
+    res.render('favorites', {pokemons})
+  })
 });
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   res.send(req.body);
 });
+
 
 module.exports = router;
