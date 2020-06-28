a6318bcce880bc2c99d35cb94182e3b518494794
jimmygranadino <jimmy.granadino@gmail.com>
2020-06-27 21:38:34 -0700

    can add records to table in db using testDb.js now

--- testDb.js
+++ testDb.js
@@ -1,10 +1,23 @@
 const db = require('./models')
 
 const errorHandler = error => {
-    console.log(🤬🤬🤬)
+    console.log('🤬🤬🤬')
 }
 
-// CREATE 
-// READ
+// CREATE - creates 1 record in specified table of db (db.[Table Name])
+db.pokemon.create({
+    name: 'Squirtle',
+    type: 'Water'
+}).then(function(poke) {
+    console.log(`🌊 Created: ${poke.name} which is a ${poke.type} type! 🌊`)
+}).catch(errorHandler)
+
+
+// READ - finds all records in specified table of db (db.[Table Name])
+// db.pokemon.findAll().then(function(poke) {
+//     console.log('Found: ', poke.name. poke.type)
+// })
+
+
 // UPDATE
 // DELETE
