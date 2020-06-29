b12c6ac37d82f54a915cff5889adb27d77e8c433
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-28 21:30:33 -0700

    styling done

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     var pokemon = apiResponse.data.results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const axios = require('axios'); 
+
+
+const errorHandler = error => {
+  console.log('WARNING: this is an error');
+  console.log(error);
+}
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
+  db.pokemon.findAll().then(pokemon => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  res.render('pokemon/index', {pokemon});
+}).catch(errorHandler)
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(pokemon => {
+    console.log(pokemon)
+    res.redirect('/pokemon')
+  }).catch(errorHandler)
 });
 
+//GET /pokemon/:id - display/show more info on each pokemon
+
+router.get('/:name', (req, res) => {
+  // db.pokemon.findOne({
+  //   where: {
+  //     id: req.params.id
+  //   }
+  // }).then(pokemon => {
+  //   console.log('got pokemon from db')
+  //   var pokemonData = 'https://pokeapi.co/api/v2/pokemon/' + pokemon.name.toLowerCase()
+  //   axios.get(pokemonData).then(response => {
+  //     console.log('got data from api')
+  //     res.render('pokemon/show', { response: response.data })
+  //   }).catch(errorHandler)
+
+  // })
+  var pokemonData = 'https://pokeapi.co/api/v2/pokemon/' + req.params.name.toLowerCase()
+  axios.get(pokemonData).then(response => {
+        console.log('got data from api')
+        res.render('pokemon/show', { response: response.data })
+      }).catch(errorHandler)
+});
 module.exports = router;
