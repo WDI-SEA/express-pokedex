ff90ac6def0bc461d75b4427a756ce43edef12fe
sinav <sinavameghi@gmail.com>
2020-07-08 11:38:40 -0700

    MVP

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+const axios = require('axios');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.render("/pokemon/index.ejs");
+  db.pokemon.findAll().then(function(poke) {
+    res.render("pokemon/index", {poke:poke})
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(([pokemon, created]) => {
+    res.redirect('/pokemon')
+  })
 });
 
+
+router.get('/:id', function(req, res) {
+  db.pokemon.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(pokemon) {
+    axios.get('http://pokeapi.co/api/v2/pokemon/'+pokemon.name.toLowerCase())
+    .then(function(response) {
+      res.render('pokemon/show', {response})
+    })
+  })
+})
+
+
+
+
 module.exports = router;
+
+
+
