a2b8ad358754f9108543d169fbc59e4b5f22ddc7
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-07-14 03:27:37 -0500

    added css styling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios'); 
+const methodOverride = require('method-override')
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +10,13 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(methodOverride('_method'))
+
+app.use(express.static(__dirname + '/public'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     var pokemon = apiResponse.data.results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+const axios = require('axios'); 
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -13,8 +15,42 @@ router.get('/', function(req, res) {
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  
-  res.redirect('pokemon');
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(function() {
+    res.redirect('/pokemon');
+  })
 });
+
+// in the route path, we could do anything for it :id or anything else
+// router.get('/:name', function(req, res) {
+//   //req.params.name is the parameter that you're passing through to the API call
+//   db.pokemon.findOne({
+//     where: {
+//       name: req.params.name
+//     }
+//   }).then(function(pokemon){
+//     var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${(req.params.name)}.toLowerCase()}`;
+//     axios.get(pokemonUrl).then(function(apiResponse) {
+//       pokemon = apiResponse.data;
+//       console.log(pokemon);
+//       res.render('pokemon/show', {pokemon});
+//     })
+//   }).catch(function(error) {
+//     console.log('💩💩💩💩💩💩💩💩💩💩');
+//     console.log(error);
+//   })
+// })
+
+router.get('/:name', (req, res) => {
+  var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${(req.params.name).toLowerCase()}`;
+  // Use request to call the API
+  axios.get(pokemonUrl).then(function(apiResponse) {
+    var pokemon = apiResponse.data;
+    res.render('pokemon/show', {pokemon})
+  })
+})
 
 module.exports = router;
