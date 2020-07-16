92d044aaf1f871a84580646bc6f93d60b27a2472
Charles Perry <charles@Charless-MacBook-Pro.local>
2020-07-15 20:39:04 -0700

    went back and finished this homework. Was a lot easier after P2.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var pokemon = require('../models/pokemon');
+var axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  let faves = []
+    db.pokemon.findAll().then(pokemons => {
+      pokemons.forEach(pokemon => {
+        faves.push(pokemon.dataValues);
+      })
+    })
+  res.send('pokemon.ejs', {pokemon: faves});
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  newFav = req.body.name
+  db.pokemon.findOrCreate({
+    where: {name: newFav}
+  }).then(([pokemon, created]) => {
+    res.redirect('/pokemon')
+  }).catch(err => {
+    console.log(err)
+  })
 });
+
+router.get('/:id', (req, res) => {
+  db.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(pokemon =>{
+    var pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+    axios.get(pokemonUrl).then(function(apiResponse) {
+      let pokeData = apiResponse.data
+      res.render('../views/show', {pokemonName: pokemon.name, pokeData: pokeData})
+    })
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
+
 
 module.exports = router;
