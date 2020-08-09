b684c6bdb2a5593985ed2f5ccaacd3ff442ed1ec
alpha-martinez <martinezalphaa@gmail.com>
2020-08-08 22:07:58 -0400

    fix a tag

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,24 +5,56 @@ const axios = require('axios');
 
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/',async (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  const locatePoke = await db.pokemon.findAll() 
-  res.render('favorites', { pokemon: locatePoke });  
+router.get('/', async (req, res) => {
+  try {
+    // TODO: Get all records from the DB and render to view
+    const locatePoke = await db.pokemon.findAll() 
+    res.render('favorites', { pokemon: locatePoke });
+  } catch (error) {
+
+    console.log(error, 'error');
+
+  }
+    
 });
 
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', async (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  await db.pokemon.findOrCreate({
-    where: {
-      name: req.body.name,
-    }
-  });
-  res.redirect('/pokemon');
+  try {
+    // TODO: Get form data and add a new record to DB
+    await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name,
+      },
+    });
+    res.redirect('/pokemon');
+  } catch (error) {
+
+    console.log(error, 'error');
+  }
+  
 });
+
+// GET/:ID
+router.get('/:name', async (req, res) => {
+  try {
+    if(req.params && req.params.name) {
+      const webURL = `https://pokeapi.co/api/v2/pokemon/${req.params.name.toLowerCase()}`;
+      const result = await axios.get(webURL);
+      let webData = result.data; 
+      res.render('show', { pokedata: webData });
+    }
+
+  } catch (error){
+
+    console.log('error');
+
+  }
+
+});
+
+//Delete /pokemon 
 
 module.exports = router;
 
-//Delete /pokemon 
