f032bea0b17063091e0bbd06672752b230924684
alpha-martinez <martinezalphaa@gmail.com>
2020-08-08 20:25:08 -0400

    first two are working

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,23 +1,28 @@
-var express = require('express');
-var router = express.Router();
-let db = require('../models');
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const axios = require('axios');
 
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  db.pokemon.findAll()
-  .then(pokemons => {
-    
-   
-    res.render('../views/favorites');
-  })
+router.get('/',async (req, res) => {
   // TODO: Get all records from the DB and render to view
+  const locatePoke = await db.pokemon.findAll() 
+  res.render('favorites', { pokemon: locatePoke });  
 });
 
+
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  await db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name,
+    }
+  });
+  res.redirect('/pokemon');
 });
 
 module.exports = router;
+
+//Delete /pokemon 
