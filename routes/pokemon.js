5624ebce13aea071c7279a7aba4108e3be09e56a
Your Name <you@example.com>
2021-01-14 11:15:52 -0800

    Created a pokemon.ejs file for showing our pokemon, in this case our favoite pokemon.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,13 +4,15 @@ const router = express.Router();
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  // res.send('Render a page of favorites here');
+  res.render('pokemon.ejs');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  // res.send(req.body);
+  res.redirect('/pokemon')
 });
 
 module.exports = router;
