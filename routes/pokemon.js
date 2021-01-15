b77c5a50bf5f347aa17259c18378f604d3171918
unknown <dancesfire@gmail.com>
2021-01-15 10:14:42 -0800

    pretty good function

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,24 +1,48 @@
+
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const axios = require('axios')
+
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.render('');
+router.get('/', function (req, res) {
+
+  db.pokefav.findAll().then(pokemons => {
+    res.render('pokemon/index.ejs', { pokemons: pokemons })
+  })
+})
+
+
+router.post('/', function (req, res) {
+  // TODO: Get form data and add a new record to DB
+  db.pokefav.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  })
+    .then(() => {
+      console.log(req.body.name)
+      res.redirect('/pokemon')
+    }
+    ).catch((error) => {
+      console.log(error)
+    })
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  db.pokefav.create(req.body)
-  .then(() => {
-    console.log(req.body)
-    res.redirect('/pokemon')
-  }
-  ).catch((error) => {
-    console.log(error)
+router.get('/:id', (req, res) => {
+  db.pokefav.findOne({
+    where: {
+      id: req.params.id
+    }
   })
-});
+    .then(poke => {
+      let pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${poke.name}`;
+      axios.get(pokemonUrl).then(apiResponse => {
+        let pokemon = apiResponse.data
+        res.render('pokemon/show.ejs', { pokemon: pokemon })
+      })
+    })
+})
 
 module.exports = router;
