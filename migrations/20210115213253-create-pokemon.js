0412e49902bc0da14ce0bb46cbfba9ee02de12e5
austintip <austin.d.tipograph@gmail.com>
2021-01-15 18:30:04 -0500

    favorites list working and has links attached

--- migrations/20210115213253-create-pokemon.js
+++ migrations/20210115213253-create-pokemon.js
@@ -9,7 +9,6 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
+db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
+  db.pokemon.findAll()
+  .then((pokes) => {
+    res.render('pokemon/index', { pokemon:pokes });
+  });
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
+  db.pokemon.create({ name:req.body.name })
+  .then((pokemon) => {
+    res.redirect('/');
+    console.log(`${pokemon.name} was added to favorites!`)
+  }).catch((err) => {
+    console.log(err)
+  });
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
 });
 
 module.exports = router;
