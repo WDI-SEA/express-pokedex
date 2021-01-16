c6254d57dd214d8af9aa173e8ded7dbbae72e682
austintip <austin.d.tipograph@gmail.com>
2021-01-15 19:46:59 -0500

    everything is working linked css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const path=require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 db = require('../models');
+const axios = require('axios'); 
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -21,6 +22,19 @@ router.post('/', function(req, res) {
     console.log(err)
   });
   // TODO: Get form data and add a new record to DB
+});
+
+router.get('/:name', (req, res) => {
+  // name api request variable
+  let pokemonName = req.params.name;
+  let pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  axios.get(pokemonUrl).then(apiResponse => {
+    let pokemon = apiResponse.data;
+  //pull info from api
+  // put url and concatenate pokemon var into it
+  // need req.params.name
+  res.render('pokemon/details', { pokemon })
+})
 });
 
 module.exports = router;
