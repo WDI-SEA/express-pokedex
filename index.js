ea2d75cb22947cb601095d4b907a04fd7b1ed10d
kys3923 <kys3923@gmail.com>
2021-01-16 00:40:21 -0500

    started working on getting favorite data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { response } = require('express');
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require('path');
+const db = require('./models')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -11,17 +13,16 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
     let pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
-  })
+  }) 
 });
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
 
-app.listen(port, () => {
-  console.log('...listening on', port );
-});
+var server = app.listen(process.env.PORT || 3000, () => console.log("server is working"));
--- migrations/20210116013127-create-pokemon.js
+++ migrations/20210116013127-create-pokemon.js
@@ -9,7 +9,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
