485b1dd608df413b5447f6c2dcb8b3cd58e939e5
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2021-01-16 12:03:07 -0800

    Updated pokemon app. Need to add delete function.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models');
+const { response } = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,13 +13,40 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
     let pokemon = apiResponse.data.results;
+    console.log(apiResponse.data.results)
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
 });
+
+app.get('/pokemon', (req, res) =>{
+  db.Pokemon.findAll().then(pokemon =>{
+    res.render('Pokemon/index', {pokemon})
+  })
+})
+
+app.post('/pokemon', (req, res) =>{
+  console.log(req.body)
+  db.Pokemon.create({
+    name: req.body.name
+  }).then(createdFav =>{
+    res.redirect('/pokemon')
+  })
+})
+
+// app.get('/pokemon/:id', (req, res) =>{
+//   axios.get(`https://pokeapi.co/api/v2/${req.params.id}`)
+//   }).then(response =>{
+//     console.log(response.data)
+//     res.render('details')
+//   }).catch(error => {
+//     console.log('Error! Please reload the page.')
+//     res.sendStatus(500)
+//   })
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,40 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
+
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(pokemon => {
+    res.send('Render a page of favorites here')
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/:id', function (req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  })
+    .then(() => {
+      res.redirect('/pokemon')
+    })
+    .catch(error => console.log(error))
 });
+
+router.get('/:name', (req, res) => {
+  // console.log(req.params.name)
+  let pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${req.params.name}`
+  axios.get(pokemonUrl).then(apiResponse => {
+    let pokemon = apiResponse.data
+    console.log(pokemon)
+    res.render('Pokemon/show', {pokemon: pokemon})
+    // res.json(pokemon)
+  }).catch(error => console.log(error))
+})
 
 module.exports = router;
