c6065f1f986c529bb9b1e3650595e694ae05af10
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-19 02:34:17 -0800

    Added Get pokemons

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  //res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(pokemons=>{
+    res.render('pokemon/index',{pokemon: pokemons})
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(poke=>{
+    console.log('Added this pokemon to Favs: '+poke.name)
+
+  })
+  
 });
 
 module.exports = router;
