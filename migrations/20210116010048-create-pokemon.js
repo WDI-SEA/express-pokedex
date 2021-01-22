24e6ddd57615c229a3b4588cf4b1e2f8e67eb784
edgard Jara <edv06jara@gmail.com>
2021-01-21 21:11:30 -0800

    rendered sprites

---
+++ migrations/20210116010048-create-pokemon.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('pokemons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('pokemons');
+  }
+};
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,41 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router();
+const db = require("../models")
 
-// GET /pokemon - return a page with favorited Pokemon
+// GET /pokemon - return a page with favorite Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  // access the data from pokemons database
+  db.pokemon.findAll()
+  .then( pokemon => {
+    res.render('favorites', {pokemon:pokemon});
+
+  })
 });
+router.get('/:name', function(req,res) {
+  let pokemonName = req.params.name;
+  axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+  .then(apiResponse => {
+    console.log(apiResponse.data)
+    res.render('show', {pokemonData:apiResponse.data})
+  })
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  //db.pokemon this is how I access the pokemon model
+  console.log(req.body.name)
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+
+    }
+  }).then(([poke, pokemonCreated]) => {
+    console.log('Created: ', pokemonCreated)
+    res.redirect('/pokemon');
+  })
 });
 
 module.exports = router;
