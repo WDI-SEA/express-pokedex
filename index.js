57371872092e375d7e9f88457932bf697c93395d
Brian Young <berkshirehunt@gmail.com>
2021-06-05 14:40:57 -0700

    favourites

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
+const express = require("express");
+const axios = require("axios");
+const ejsLayouts = require("express-ejs-layouts");
+// const rowdy = require("rowdy-logger");
+// const rowdyResults = rowdy.begin(app);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + "/public/"));
+
+// const db = require("/models");
 
 // GET / - main index of site
-app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+app.get("/", (req, res) => {
+  let pokemonUrl = "http://pokeapi.co/api/v2/pokemon/?limit=50";
   // Use request to call the API
-  axios.get(pokemonUrl).then(apiResponse => {
+  axios.get(pokemonUrl).then((apiResponse) => {
     let pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
-  })
+    res.render("index", { pokemon: pokemon.slice(0, 151) });
+  });
 });
 
 // Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+app.use("/pokemon", require("./routes/pokemon"));
 
 app.listen(port, () => {
-  console.log('...listening on', port );
+  console.log("...listening on", port);
+  // rowdyResults.print();
 });
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
-// GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get("/", (req, res) => {
+  async function findPoke() {
+    try {
+      const findPoke = await db.pokemon.findAll();
+      // res.send(findPoke)
+      res.render("favorites", { pokemon: findPoke });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  findPoke();
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post("/", function (req, res) {
+  //posts are mounted on the req body object
+  let pokeName = req.body.name;
+  console.log(pokeName);
+  async function findOrCreatePokemon() {
+    try {
+      // the findOrCreate promise returns an array with two elements,
+      // so 'array destructuring' is used to assign the names to the elements
+      const [pokemon, created] = await db.pokemon.findOrCreate({
+        // where is used search for values in columns
+        where: {
+          name: pokeName,
+        },
+      });
+      console.log(`${pokemon.name} was ${created ? "created" : "found"}`);
+      res.redirect("/pokemon");
+      // res.json({ username: "Flavio" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  findOrCreatePokemon();
 });
 
+// DELETE favorite pokemon
+router.delete("/pokemon:name", (req, res) => {
+  let removePokemon = req.body.name;
+  async function deletePokemon() {
+    try {
+      // delete pokemon 'removePokemon'
+      // const [pokemon, ...] = await db.pokemon.
+      const [pokemon, removePokemon] = await db.pokemon.destroy({
+        where: { name: removePokemon },
+      });
+      console.log(await db.pokemon.all());
+      console.log(`💀 Name of removePokemon: ${removePokemon}`);
+    } catch (err) {
+      console.log(err);
+    }
+    deletePokemon();
+  }
+});
 module.exports = router;
