64e50480d6e880dd3c332b523c1beea1266e31e1
Consecratio <jcasique@gmail.com>
2021-06-04 17:03:17 -0700

    favorites working, need to add more pokemon details to pokemon/show.ejs

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,43 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+            .then(pokemons => {
+              console.log(pokemons)
+              res.render('pokemon/index.ejs', { pokemons: pokemons})
+            })
+            .catch(err => {
+              console.log(err)
+            })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create({
+    name: req.body.name
+  }).then(poke => {
+    console.log('Created: ', poke.name)
+    res.redirect('pokemon/')
+  }).catch(err => {
+    console.log(err)
+  })
 });
+
+// GET /pokemon/:name - renders the show page with information about the pokemon
+router.get('/:name', (req, res) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+        .then(resFromAPI => {
+          const pokemon = resFromAPI.data
+          res.render('pokemon/show', { pokemon: pokemon })
+        }).catch(err => {
+          console.log(err)
+        })
+})
 
 module.exports = router;
