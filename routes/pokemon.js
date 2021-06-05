69756cfe5260b115b3ba0d897c720b75f14e1ef0
BeckyBoo88 <rbooco07@gmail.com>
2021-06-04 18:07:20 -0600

    trying to get the basics

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  res.render('pokemon/index')
+  // res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
