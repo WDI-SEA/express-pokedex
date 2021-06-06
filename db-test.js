8ae052f407c0a9dbb5e7c4e6267b984f2487ea6a
rei-bp <reinard.bp@gmail.com>
2021-06-06 12:55:29 -0700

    load favorite pokemon - add to favorite pokemon solve

---
+++ db-test.js
@@ -0,0 +1,12 @@
+// const db = require('./models');
+
+// db.pokemon.create({
+//     name: 'pikachu'
+//   }).then(poke => {
+//     console.log('Created: ', poke.name)
+//   })
+
+// db.pokemon.findAll()
+// .then(poke => {
+//   console.log(poke)
+// })
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,10 +18,12 @@ app.get('/', (req, res) => {
     let pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
-});
+})
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
+
+
 
 app.listen(port, () => {
   console.log('...listening on', port );
