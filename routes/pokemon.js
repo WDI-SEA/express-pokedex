f2ab6397971d5d9ab746cfdb0239946f53058430
BeckyBoo88 <rbooco07@gmail.com>
2021-06-06 12:40:58 -0600

    trying to get favs up

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,17 +1,46 @@
 const express = require('express');
-const router = express.Router();
+let router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.render('pokemon/index')
-  // res.send('Render a page of favorites here');
+  async function findAllPokemon() {
+    try {
+      const pokemon = await db.pokemon.findAll()
+      console.log(pokemon);
+      res.render('pokemon/favorites', { pokemon : pokemon })
+      // res.send(pokemon)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  findAllPokemon()
+
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+  let pokeName = req.body.name
+  async function findOrCreatePokemon(){
+    try {
+      // the findOrCreate promise returns an array with two elements,
+      // so 'array destructuring' is used to assign the names to the elements
+      const [pokemon, created] = await db.pokemon.findOrCreate({
+        // where is used search for values in columns
+        where: {
+          name: pokeName
+        }
+          })
+      console.log(`${pokemon.name} was ${created ? 'created' : 'found'}`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  findOrCreatePokemon()
 
-module.exports = router;
+  res.redirect("/pokemon")
+})
+
+
+module.exports = router
