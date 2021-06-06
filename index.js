d8a9ff5a4e027f1ae9ebcbba91611eeb1dcfdf97
Benji <bwolfenberger@gmail.com>
2021-06-06 15:20:12 -0700

    added method override functionality

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const axios = require('axios'); 
+const express = require('express')
+const axios = require('axios')
 const ejsLayouts = require('express-ejs-layouts');
 const method_override = require('method-override')
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'))
 app.use(express.urlencoded({ extended: false }));
-app.use(ejsLayouts);
-app.use(method_override)
+app.use(ejsLayouts)
+app.use(method_override('_method'))
 
 // GET / - main index of site
 app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=100';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151'
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
     let pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
-});
+})
 
 // Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+app.use('/pokemon', require('./routes/pokemon'))
 
 app.listen(port, () => {
   console.log('...listening on', port );
-});
+})
