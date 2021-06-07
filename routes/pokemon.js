66db11f00eaed36bf47e30e71719d16b684a5f10
Anna Zocher <anna.zocher@gmail.com>
2021-06-07 12:01:47 -0700

    final reqs solved

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 let db = require("../models")
+const axios = require('axios')
+
+router.get('/:id', (req, res) => {
+  // get the name of the pokemon associated with this id from our db
+    // get name from req.query
+    let name = req.query.name
+    
+  // get extra information for pokemon at id
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((apiRes) => {
+
+        // info we want to get: pokeAPI id, pokeAPI char image, types
+        let imgSrc = apiRes.data.sprites.front_default
+        let pokeId = apiRes.data.id
+        let types = apiRes.data.types[0].type.name
+
+        res.render('pokemon/show', {src: imgSrc, id: pokeId, type: types})
+      })
+      .catch(err => console.log(err))
+    
+  // render a page with pokemon details passed back from our APIcall
+})
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
