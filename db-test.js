b2c16c731f0f00bee3adc61a8b78e3f5ff966223
miknayr <teamry4n@gmail.com>
2021-06-07 10:58:02 -0700

    styled the grid a little

--- db-test.js
+++ db-test.js
@@ -1,17 +1,15 @@
 const db = require('./models');
 
-db.pokemons.create({
+db.pokemon.create({
     name: 'pikachu',
     img_url:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
   }).then(poke => {
     console.log('Created: ', poke.name)
+    db.pokemon.findOne({
+      where: {
+        name: poke.name;
+      }
+    }).then(pokeFound => {
+      console.log('Found: ', pokeFound.name)
+    })
   })
-
-db.pokemons.findOne({
-  where: {
-    name: 'pikachu'
-  }
-}).then(poke => {
-  console.log('Found: ', poke.name)
-})
-
