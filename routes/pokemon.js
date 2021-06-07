e189fa08e0ab935b8adaa3dc05e61b5a3dd17787
erikwinkel <erikpwinkel@gmail.com>
2021-06-07 09:04:46 -0700

    add routes and views

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,35 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(pokemons => {
+      console.log(pokemons)
+      res.render('pokemon/index', { pokemons })
+    })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(pokemon => {
+    res.redirect('/pokemon')
+  })
 });
+
+// GET /pokemon/:name renders a shwo page with information about that pokemon
+router.get('/:name', (req,res) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+    .then(apiResponse => {
+      console.log(apiResponse.data)
+      res.render('pokemon/show', { pokemon: apiResponse.data})
+    })
+})
 
 module.exports = router;
