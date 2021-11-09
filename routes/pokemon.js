61b25990f83b0072c851aafdc27d3816b9313b28
steven43533 <steven43533@gmail.com>
2021-11-08 21:40:52 -0500

    Finished part 4

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models')
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
@@ -14,6 +15,50 @@ router.get('/', (req, res) => {
     })
   //res.send('Render a page of favorites here');
 });
+router.get('/:name', function(req,res) {
+
+  //we used this console log, to check out our request object
+  //console.log('this is req', req.query);
+  let name = req.params.name
+  // console.log('this should be the movie title', imdbId);
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(apiRes => {
+      //console.log('this is apiRes.data', apiRes.data);
+          let name = apiRes.data.name
+          let height = apiRes.data.height
+          let weight = apiRes.data.weight
+          //let abilities = apiRes.data.abilities[2].ability.name
+          let pokemonImage = apiRes.data.sprites.front_default
+  
+          //res.render results to reults.ejs with our selected data sent as an object
+          res.render('faveDetail', {name: name,
+            weight: weight, 
+            height: height,
+            pokemonImage: pokemonImage
+            })
+      })
+      .catch(err => {
+          console.log(err);
+      })
+  
+  })
+
+router.get('/:name', (req,res) => {
+  db.pokemon.findOne({
+    where: {name: req.params.name}
+  })
+  .then(foundFave => {
+    //let abilities = foundFave.data.abilities
+    res.render('faveDetail', {name: foundFave.name,
+      abilities: foundFave.ability,
+      weight: foundFave.weight, 
+      height: foundFave.height
+      })
+  })
+  .catch(error => {
+    console.log(error);
+  })
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
@@ -23,7 +68,7 @@ router.post('/', (req, res) => {
     name: data.name,
   })
   .then(createFave => {
-    res.redirect(`/pokemon`)
+    res.redirect(`/pokemon/${createFave.name}`)
   })
   .catch(error => {
     console.log(error);
@@ -32,5 +77,11 @@ router.post('/', (req, res) => {
     console.log(createdPokeFave);
   })
 });
+
+
+  
+
+
+
 
 module.exports = router;
