99ab991e06e70193a21ca60ca9b73e80df8e6883
hannahlegros <hannahrlegros@gmail.com>
2021-11-09 09:13:28 -0500

    stuck

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(poke =>{
+      // console.log('this is fave', poke)
+      res.render('/pokemon/index', {faves: poke})
+    })
+    .catch(error =>{
+      console.error
+    })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/addFave', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+    const data = JSON.parse(JSON.stringify(req.body))
+    res.send(req.body);
+    db.pokemon.create({
+      name: data.name
+    })
+    .then(createdFave =>{
+      console.log('fave created', createdFave)
+      res.redirect(`/faves/${createdFave.name}`)
+    })
+    .catch(error =>{
+      console.error
+    })
 });
+
+// SHOW 
+
 
 module.exports = router;
