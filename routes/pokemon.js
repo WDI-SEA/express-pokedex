84a8bcc0f8885be07834c75af1b37e6ed0649cef
mgkdn9 <mkohlberg95@gmail.com>
2021-11-09 07:30:01 -0600

    retrieving favorites and showing on screen

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const router = express.Router();
+const { default: axios } = require('axios')
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
-});
+  db.pokemon.findAll()
+    .then(faves => {
+      // res.render('indexFaves', {results: faves})
+      res.render('pokemon/index',{favorites:faves})
+    })
+    .catch(hi => {
+      console.error
+    })
+  // console.log(db.pokemon)
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+  res.send(req.body)
+})
 
-module.exports = router;
+module.exports = router
