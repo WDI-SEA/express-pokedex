0a6d3481e2c0814b5bdeede028b703e3023b1cb2
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-11-08 20:24:08 -0600

    finished homework.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const axios = require('axios');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then((pokemon) => {
+    res.render('index.ejs', { pokemon });
+  });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon
+    .create({
+      where: {
+        name: req.body.name,
+      },
+    })
+    .then(() => {
+      res.redirect('/pokemon');
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+});
+
+//Display more info on each Pokemon
+router.get('/:name', (req, res) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`).then((apiResponse) => {
+    res.render('show', { pokemon: apiResponse.data });
+  });
 });
 
 module.exports = router;
