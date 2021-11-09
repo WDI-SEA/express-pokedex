4630e7061d4c97435bfaf25e0cc2cff8e13d5e4b
DG-98 <dgarconpro@gmail.com>
2021-11-08 21:55:23 -0500

    Derrick G completed deliverable

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,64 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
+const axios = require("axios")
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
+  db.pokemon
+    .findAll()
+    .then((faves) => {
+      res.render("indexFaves", { results: faves })
+    })
+    .catch((error) => {
+      console.error
+    })
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
+  const data = JSON.parse(JSON.stringify(req.body))
+  console.log("this is data", data)
+   db.pokemon
+     .create({
+       name: data.name
+     })
+     .then(() => {
+       res.redirect('/pokemon')
+     })
+     .catch(error => {
+       console.log(error)
+     })
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+})
+
+router.get('/:name', function (req, res) {
+  let pokeInfo = req.params.name
+
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeInfo}`)
+  .then((apiRes) => {
+    let name = apiRes.data.name 
+    let height = apiRes.data.height
+    let weight = apiRes.data.weight 
+    let base_experience = apiRes.data.base_experience
+    let forms = apiRes.data.forms 
+    let sprites = apiRes.data.sprites.front_default
+
+    res.render("faveDetail", {
+      height: height,
+      weight: weight,
+      base_experience: base_experience,
+      forms: forms,
+      name: name,
+      sprites: sprites
+    })
+  })
+  .catch((err)=>{
+    console.error 
+  })
+})
+
+
 
 module.exports = router;
