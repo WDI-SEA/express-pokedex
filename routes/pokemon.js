fa6590579ae432451621bb1af3b11555f8c56516
DagemBefikadu <befikadu.dagem@gmail.com>
2021-11-08 22:52:16 -0800

    Everything works need to add styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,76 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
+const axios = require('axios')
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(faves => {
+      res.render('indexFaves',{results: faves})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  // res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  const data = JSON.parse(JSON.stringify(req.body))
+  console.log('this is data', data)
+  db.pokemon.create({
+    name: data.name
+  })
+  .then(newFave => {
+    console.log('db instance created: \n', newFave)
+    res.redirect('/pokemon')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  // res.send(req.body);
 });
+
+router.get('/:name', function(req,res) {
+  let pokemonContent = req.params.name
+
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonContent}/`)
+    .then(apiRes => {
+      let pokemonName = apiRes.data.name
+      let pokemonAbilities =  apiRes.data.abilities[0].ability.name
+      let pokemonBaseStat = apiRes.data.stats[0].base_stat
+      let pokemonTypes = apiRes.data.types[0].type.name
+      let pokemonImg = apiRes.data.sprites.other.dream_world.front_default
+      let pokemonMove = apiRes.data.moves[0].move.name
+
+      res.render('faveDetail', { pokemonName, pokemonAbilities, pokemonBaseStat, pokemonTypes, pokemonImg, pokemonMove})
+    })
+    .catch(error =>  {
+      console.log(error)
+    })
+})
+
+//we'er going to add a delete, that allow us to remove a fav
+router.delete('/:name', (req, res) => {
+  // console.log('this is the id\n', req.params.id)
+  db.pokemon.destroy ({
+      where: {name: req.params.name}
+  })
+  .then(deletedItem => {
+      // destroy returns 1 if something is deleted and 0 if nothing happens
+      // console.log('You deleted:  ', deletedItem)
+      res.redirect('/pokemon')
+  })
+  .catch(error => {
+      console.log(error)
+  })
+})
+
+
+
 
 module.exports = router;
