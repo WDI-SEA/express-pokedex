974c9a0da6c11d28fb90d9e04ee0bbf166d4d01f
steven43533 <steven43533@gmail.com>
2021-11-08 20:14:21 -0500

    Finished steps 1-3

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+    .then(faves => {
+      res.render('indexFaves', {results: faves})
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  //res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  const data = JSON.parse(JSON.stringify(req.body))
+  db.pokemon.create({
+    name: data.name,
+  })
+  .then(createFave => {
+    res.redirect(`/pokemon`)
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  .finally(createdPokeFave => {
+    console.log(createdPokeFave);
+  })
 });
 
 module.exports = router;
