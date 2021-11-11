17e154aa983100001449f9f2c0792e782607ce58
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-11-10 21:54:23 -0500

    completed part 3 get pokemon favorites to render

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then(poke => {
+    res.render('favorite.ejs', {results: poke})
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
