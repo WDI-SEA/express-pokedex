4aad96bc5fcec3f34cebfe0c00f3e8f3299860e1
gsdkira <larreakelly@gmail.com>
2021-12-22 17:56:14 -0500

    Pokedex complete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+const axios = require('axios')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   db.pokemon.findAll()
-  .then(pokemon =>{
-    res.render('indexPokemon', {results:pokemon})
+  .then(poke =>{
+    res.render('favorite', {results: poke})
   })
   .catch(error => {
     console.error
   })
   // TODO: Get all records from the DB and render to view
-//   res.send('Render a page of favorites here');
+  // res.send('Render a page of favorites here');
 })
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/pokemon', (req, res) => {
+router.post('/', (req, res) => {
   const data = JSON.parse(JSON.stringify(req.body))
-  // console.log('this is data', data)
+  console.log('this is data', data)
   db.pokemon.create({
-    name: forms.name
+    name: data.name
   })
   .then(createdFave =>{
-    res.redirect(`/pokemon/${createdFave}`)
+    res.redirect('/pokemon')
   })
   .catch(error => {
     console.error
@@ -32,18 +33,33 @@ router.post('/pokemon', (req, res) => {
 
 })
 
-router.get('/:id', (req, res) => {
-  console.log('this is the fave id\n', req.params.id)
-  db.pokemon.findFour({
-     where: { id: req.params.id } 
-  })
-  .then(foundFave => {
-      res.render('index', { name: foundFave.pokemon.name})
+router.get('/:name', (req, res) => {
+  const pokeName = req.params.name
+  console.log('this is pokeName', pokeName)
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
+  .then(apiRes => {
+      const image = apiRes.data.sprites.other.dream_world.front_default
+      const abilities = apiRes.data.abilities
+      const moves = apiRes.data.moves
+      const stats = apiRes.data.stats
+      const name = req.params.name
+      // res.render(JSON.parse(JSON.stringify(apiRes.data)))
+      res.render('pokeDetail', {name: name, abils: abilities, image:image, moves: moves, stats: stats})
   })
   .catch(error => {
       console.error
   })
 })
 
-
+router.delete('/:name', (req,res) =>{
+  db.pokemon.destroy({
+    where: {name: req.params.name}
+  })
+  .then(deletedItem =>{
+    res.redirect('/pokemon')
+  })
+  .catch(err=>{
+    console.err
+  })
+})
 module.exports = router
