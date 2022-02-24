2dba7a9e51508beaa8235083a93c0b0ba39eff70
kokopuffz <chanheeya@gmail.com>
2022-02-24 04:10:08 -0600

    broken pages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
-
+const db = require("./models");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,13 +11,21 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=100';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
     let pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render('index', { pokemon: pokemon.slice(0, 100) });
   })
 });
+// app.get('/pokemon', async (req, res)=>{
+//   try{
+//     const faves = await db.pokemon.findAll()
+//     res.render(faves)
+//   }catch(err){
+//     console.log(err)
+//   }
+// })
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,52 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router();
+const db = require("../models");
+
+
+
+
+router.get('/:name',(req, res)=>{
+  let name = req.query.name
+  console.log(name)
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  .then((apiResponse)=>{
+    let pokemon = apiResponse.data;
+    res.render('show.ejs', {pokemon: pokemon})
+  })
+})
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) => {
   // TODO: Get all records from the DB and render to view
   res.send('Render a page of favorites here');
 });
-
+router.get("/", (req, res) => {
+  db.pokemon.findAll();
+  .then (pokes =>{
+    res.render("/", { pokemon: pokeFaves });
+  })
+    
+  } catch (err) {
+    // console.log(err);
+  }
+});
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  try{
+    let poke = req.body.name
+    const checkAndAdd = await db.pokemon.findOrCreate({ 
+      where: {
+        name: poke
+      },
+    })
+
+      res.redirect('/pokemon')
+  }catch(error){
+    // console.log(error)
+  }
 });
 
 module.exports = router;
