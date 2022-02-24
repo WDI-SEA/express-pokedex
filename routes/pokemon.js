1bfa0c55c83ca71d4b25a9af9cbae60686148767
br1anlee <joonmolee3@gmail.com>
2022-02-23 21:14:23 -0800

    finished everything except the css portion

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,43 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-// GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+// GET /pokemon - return a page with favorite Pokemon
+router.get('/', async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  try {
+    const allFaves = await db.pokemon.findAll()
+    res.render('favorites.ejs', {pokemon: allFaves})
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  try {
+    await db.pokemon.create({
+      name: req.body.name
+    })
+
+    res.redirect('/pokemon')
+  } catch (error) {
+    console.log(error)
+  }
 });
+
+
+
+router.get('/:name', async (req, res) => {
+  try {
+    const response = await axios.get(`http://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+    res.render('show.ejs', {pokemonDetail: response.data})
+    // console.log(response)
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 module.exports = router;
