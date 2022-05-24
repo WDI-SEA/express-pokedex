0cfd93265a019425c97fa9154c96e758cec6a309
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-24 17:45:16 -0600

    Allow user to add favorite pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const db = require("../models")
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
-});
+  const pokemon = await db.pokemon.findAll()
+  res.render("pokemon", { pokemon })
+  // res.send("Render a page of favorites here")
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+  await db.pokemon.create(req.body)
+  res.redirect("/pokemon")
+})
 
-module.exports = router;
+module.exports = router
