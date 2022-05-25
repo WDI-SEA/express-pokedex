19555276af9e16c076b14e2d98d97dbf4d0a871c
seiplet93 <seiplet93@gmail.com>
2022-05-25 12:04:24 -0400

    deliverable complete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const db = require("../models");
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  const allFaves = await db.pokemon.findAll();
+  // render faves page
+  console.log(allFaves);
+  res.render("faves.ejs", { allFaves });
+  // res.send("faves.ejs"); //allFaves });
+  // res.send("Render a page of favorites here");
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  await db.pokemon.create({
+    name: req.body.name,
+  });
+  res.redirect("/pokemon");
 });
+// router.get("/pokemon", async (req, res) => {
+//   console.log(allFaves);
+// });
 
 module.exports = router;
