8f7234b468b97d25e12d97f44ce0277e2b12f6c9
ASelviyan <angelikaselviyan5@gmail.com>
2022-05-24 22:07:55 -0600

    STRUGGLE

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,38 @@
+const { default: axios } = require('axios');
 const express = require('express');
+const { redirect } = require('express/lib/response');
 const router = express.Router();
+const db = require("../models");
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  const pokemons = await db.pokemon.findAll()
+  res.render('pokemon/index.ejs', {pokemons});
 });
 
+
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  await  db.pokemon.create(req.body)
+  res.redirect('/pokemon')
 });
+
+//SHOW -- GET ROUTER
+router.get('/:name', async (req, res) =>{
+  let pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${req.params.name}`;
+  const pokemonData = await axios.get(pokemonUrl)
+
+  console.log(pokemonData.data)
+
+
+    // const pokemons = await db.pokemon.findAll()
+    res.render('pokemon/show.ejs', {
+      name: req.params.name,
+      pokemonData
+    })
+})
 
 module.exports = router;
