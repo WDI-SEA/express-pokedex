83a2f3987c5aed4861bcfc11fc7c85555b66e00e
weston-bailey <wdbailey87@gmail.com>
2022-09-08 14:53:39 -0700

    solves thru part 3

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get('/', async (req, res) => {
+  try {
+    // find all of the pokemon that have been favorited
+    const pokemon = await db.pokemon.findAll()
+    console.log(pokemon)
+    // render them on a favorites page
+    res.render('favorites.ejs', { favorites: pokemon })
+  } catch(err) {
+    console.log(err)
+    res.send('server error')
+  }
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post('/', async (req, res) => {
+  try {
+    // find or create the pokemon in the db
+    await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name
+      }
+    })
+    // redirect to favorites
+    res.redirect('/pokemon')
+  } catch(err) {
+    console.log(err)
+    res.send('server error')
+  }
+
 });
 
 module.exports = router;
