b6b2def486d5701b3115d390f73357e5cf891f47
curtlen88 <curtlen88@gmail.com>
2022-12-16 22:37:33 -0700

    basic setup

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get('/', async (req, res) => {
+  try {
+    // TODO: Get all records from the DB and render to view
+    //READ function to find all favorite pokemon
+    const favPokemon = await db.pokedex.findAll()
+    res.render('index.ejs', {
+      favPokemon: favPokemon
+    })
+  } catch (error) {
+    console.log(error)
+  }
 });
 
+
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const favArray = await db.pokedex.findOrCreate({
+      where: {
+        name: pokedex.name
+      }
+    })
+    
+  } catch (error) {
+    
+  }
   // TODO: Get form data and add a new record to DB
   res.send(req.body);
 });
