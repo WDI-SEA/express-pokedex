832a6c4f143a4bb4e13cb639f3c7ee6a26e359e7
Jaywomack <womackjw@hotmail.com>
2022-12-16 20:48:51 -0800

    getting 8 random images

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
- 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 app.get("/", async (req, res) => {
     try {
-        let data = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=2')
+        let data = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=8&&offset=${getRandomInt(1145)}`)
         let pokemon = await data.data.results
-        let num = pokemon[0].url.split('/').filter(c => !isNaN(c)&&c!=='')
-        console.log(Number(num));
+        
         res.render('index',{
           pokemon:pokemon  
         })
@@ -28,3 +29,6 @@ app.get("/", async (req, res) => {
 app.listen(8000, () => {
   console.log("listening for requests on port 8000");
 });
+
+// let num = pokemon[0].url.split('/').filter(c => !isNaN(c)&&c!=='')
+//         console.log(Number(num));
