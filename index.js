3c8bd1f3400894a65c162f53186ab742f00d8347
Jaywomack <womackjw@hotmail.com>
2022-12-16 20:13:59 -0800

    created logic to get pokemon number from url string

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const express = require("express");
 const app = express();
 const Pokemon = require("./Pokemon");
 const axios = require("axios");
-app.get("/", async (req, res) => {});
+const { urlencoded } = require("express");
+app.set('view engine','ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}));
+
+ 
+
+app.get("/", async (req, res) => {
+    try {
+        let data = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=2')
+        let pokemon = await data.data.results
+        let num = pokemon[0].url.split('/').filter(c => !isNaN(c)&&c!=='')
+        console.log(Number(num));
+        res.render('index',{
+          pokemon:pokemon  
+        })
+    } catch (error) {
+        // console.error(error);
+    }
+});
 
 app.listen(8000, () => {
   console.log("listening for requests on port 8000");
