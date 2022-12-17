de97927c7b449a61f5b37a5581558df954a2e061
ninambash <ninambash@gmail.com>
2022-12-17 12:04:37 -0800

    updated

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  //res.send('Render a page of favorites here');
+  
+    const favoritePokemon = await db.pokemon.findAll()
+     
+    console.log(favoritePokemon)
+  
+
+  res.render('pokemon/index',{favePokemon: favoritePokemon})
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
