ea4edcf08fe74eaba7fcd2560927bdfa413c54b9
paulyjkim1 <paulyjkim1@gmail.com>
2022-12-18 01:24:37 -0800

    finished deliverable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,12 +12,14 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
-    let pokemon = apiResponse.data.results;
+    let pokemon = apiResponse.data.results; 
+    console.log(pokemon) 
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
+  
 });
 
 // Imports all routes from the pokemon routes file
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,54 @@
 const express = require('express');
+const axios = require('axios'); 
+const ejsLayouts = require('express-ejs-layouts');
 const router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get('/', async (req, res) => {
+  // TODO: Get all records from  the DB and render to view
+  try {
+    const allFaves = await db.pokemon.findAll()
+    res.render('pokemon/index.ejs' ,{
+      allFaves: allFaves,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('api error')
+  }
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  console.log(req.body)
+  try{
+    const returnArray = await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name,
+      }
+    })
+
+    res.redirect('/pokemon')
+  }catch (err) {
+    console.log(err)
+    res.status(500).send('api error')
+  }
 });
+
+
+router.get('/:name', async (req, res) => {
+  try{
+    let pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${req.params.name}`;
+    // Use request to call the API
+    let response = await axios.get(pokemonUrl)
+    res.render('show.ejs', {
+      poke: response.data
+    })
+  }catch (err) {
+    console.log(err)
+    res.status(500).send('api error')
+  }
+})
 
 module.exports = router;
