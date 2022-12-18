1d25969515eaec2499d7bfe546a78727417ceaaf
emilyjryan <emilyryanmac@gmail.com>
2022-12-18 14:14:26 -0800

    halfway through deliverable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
-
+const db = require('./models')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,18 +9,67 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
-// GET / - main index of site
+// GET / - main index of site, render all 151 pokemon characters
 app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
-    let pokemon = apiResponse.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    let OGpokemon = apiResponse.data.results;
+    res.render('index', { OGpokemon: OGpokemon.slice(0, 151) });
   })
 });
 
+// GET //pokemon/:pokemon -- READ a specific pokemon's details
+app.get('/pokemon/:name', async (req, res) => {
+  try {
+    // url route parameters come in on the req.params
+    const url = `http://pokeapi.co/api/v2/pokemon/${req.params.name}`
+    console.log(req.params)
+    const response = await axios.get(url)
+
+    res.render('detail.ejs', {
+      name: response.data
+    })
+  } catch(err) {
+    console.log('🤬🤬🤬🤬🤬', err)
+    res.status(500).send('api error 🤬')
+  }
+})
+
+// GET //pokemon-- READ all fave pokemon characters from pokemons DB
+app.get('/pokemon', async (req, res) => {
+  try {
+    const allPokemons = await db.pokemon.findAll()
+    res.render('faves.ejs', {
+      allPokemons: allPokemons
+    })
+  } catch (err) {
+    console.log('UGH AN ERROR', err)
+    res.status(500).send('So very sorry, there\'s been an error somewhere...')
+  }
+})
+
+// POST //pokemon -- CREATE a new fave pokemon in the DB
+app.post('/pokemon', async (req, res) => {
+  try {
+    //create a new fave pokemon in the DB
+    const [pokemonCharacter, create] = await db.pokemon.findOrCreate({
+      where: {name: req.body.name}
+    })
+    console.log(pokemonCharacter)
+    console.log(create)
+    
+    // redirect to /faves to show the user their faves
+    res.redirect('/pokemon')
+  } catch (err) {
+    console.log('UGH AN ERROR', err)
+    res.status(500).send('So very sorry, there\'s been an error somewhere...')
+  }
+})
+
 // Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+// app.use('/pokemon', require('./routes/pokemon'));
+
 
 app.listen(port, () => {
   console.log('...listening on', port );
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
-});
+// // GET /pokemon - return a page with favorited Pokemon
+// router.get('/', (req, res) => {
+//   // TODO: Get all records from the DB and render to view
+//   res.render('faves.ejs');
+// });
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
