4e68c0ee6341361967fae177b025efe0f64c88da
cailinshaffer <cailinshaffer@gmail.com>
2023-01-09 23:17:37 -0700

    adding pokedex deliverable

---
+++ db-test.js
@@ -0,0 +1,37 @@
+// require your models 
+// const db = require('./models');
+
+// db.pokemon.create({
+//     name: 'pikachu'
+//    })
+//    .then(poke => {
+//      console.log('Created: ', poke.name)
+//      db.pokemon.findOne({
+//          where: {
+//            name: 'pikachu'
+//          }
+//        })
+//        .then(poke => {
+//          console.log('Found: ', poke.name)
+//        })
+//        .catch(console.log)
+//    })
+//    .catch(console.log)
+
+// // create some pokemon with async/await syntax
+// async function createPokemon() {
+//   try {
+//     const newPokemon = await db.pokemon.create({ name: 'charizard' })
+//     console.log('the new pokemon is:', newPokemon)
+//     const foundPokemon = await db.pokemon.findOne({
+//       where: {
+//         name: 'charizard'
+//       }
+//     })
+//     console.log('the found pokemon is:', foundPokemon)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+
+// createPokemon()
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,50 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  try {
+      const allFaves = await db.pokemon.findAll()
+      //console.log('Favorites', allFaves)
+      res.render('Favorites' , {pokemons: allFaves})
+  } catch (err) {
+  console.error('❌❌❌❌', err)
+  res.status(500).send('api errorz')
+}
+})
+
+
+router.post('/', async (req, res) => {
+  // res.send(req.body);
+  try {
+    const newFave = await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name
+      }
+    })
+    //console.log("New favorite:", newFave)
+    res.redirect("/pokemon")
+  } catch (err) {
+    console.error('❌❌❌❌', err)
+    res.status(500).send('api errorz')
+  }
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+// // GET /:name
+router.get('/:name', async (req, res) => {
+  try {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokeName}/`
+    const pokeName = req.params.name
+    const foundData = await axios.get(apiUrl)
+    res.render("show", {pokemon: foundData.data})
+  } catch (err) {
+    console.error('❌❌❌❌', err)
+    res.status(500).send('api errorz')
+  }
+})
+
+
 
 module.exports = router;
