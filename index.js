22ffa2aa13be2407770d8d8896ea078cce8c2304
juanedcabrera <juanedcabrera@gmail.com>
2023-04-03 22:08:54 -0600

    Getting to favorite

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
-
+const db = require('./models')
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'));
 
 // GET / - main index of site
 app.get('/', (req, res) => {
@@ -19,6 +22,30 @@ app.get('/', (req, res) => {
     res.render('index', { pokemon: pokemon });
   })
 });
+
+app.get('/pokemon', async (req, res) => {
+  try {
+    const allPokemon = await db.pokemon.findAll();
+    console.log(__dirname)
+    res.render('index.ejs', { pokemon: allPokemon });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/pokemon', async (req, res) => {
+  const { name } = req.body;
+  try {
+    const newPokemon = await db.pokemon.create({ name });
+    console.log('Created new Pokemon:', newPokemon.name);
+    res.redirect('/pokemon');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./controllers/pokemon'));
