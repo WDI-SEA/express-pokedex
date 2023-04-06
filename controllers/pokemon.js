e8c0dc0f19a7194df4ae45c423b33879ee37eb90
dxk0ta <dxkotacamps@gmail.com>
2023-04-05 20:53:01 -0700

    finally fixed my terminal and got a repository on github

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
+const axios = require('axios'); 
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get('/', async (req, res) => {
+  
+  let allFavePokemon = await db.pokemon.findAll()
+  console.log(allFavePokemon)
+
+  res.render('pokemon/index', {
+    pokemon: allFavePokemon
+  })
 });
 
+// GET /pokemon/name - return a page with info on a specific pokemon
+
+router.get('/:name', async (req, res) => {
+
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`).then(apiResponse => {
+    let pokemon = apiResponse.data
+    console.log(pokemon)
+    res.render('pokemon/show', {
+      pokemon: pokemon,
+      officialArt: pokemon.sprites.other["official-artwork"].front_default
+    })
+  })
+})
+
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post('/', async (req, res) => {
+  let newPokemon = await db.pokemon.create({name: req.body.name})
+  console.log(newPokemon)
+
+  res.redirect('/pokemon')
 });
 
 module.exports = router;
