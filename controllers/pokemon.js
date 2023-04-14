60ee60d8e4730f997d165eb5d0e8fa5c10865393
gloreea <aranda_gloree408@icloud.com>
2023-04-14 14:57:30 -0700

    get and post /pokemon

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const db = require("./models")
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TODO: Get all records from the DB and render to view
+  let allFavePokemon = await db.pokemon.findAll()
+  console.log(allFavePokemon)
   res.send('Render a page of favorites here');
 });
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', (req, res) => {
